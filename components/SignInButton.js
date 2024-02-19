4ba7839fa804c7e1d2198bc9e39fa6030d50f75e
Luke Sanger <lukesngr@gmail.com>
2024-02-20 08:13:43 +1100

    Hopefully this loops back to app

--- components/SignInButton.js
+++ components/SignInButton.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, Linking } from "react-native";
 import { useEffect } from "react";
 
 const styles = StyleSheet.create({
@@ -17,8 +17,18 @@ const styles = StyleSheet.create({
 
 export default function SignInButton() {
 
+    async function SignIn() {
+        const state = 'ipzmuRVqYj5r9LiNPks2dVsMR7XFNInHXWFYMxSC0'
+        const clientId = 'a2dfc2d05c3a866886b7'
+        const allowSignup = true
+    
+        const url=`https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}&allow_signup=${allowSignup}`
+    
+        await Linking.openURL(url);
+    }
+
     return (
-        <Pressable style={styles.signInButton} onPress={() =>  console.log("yea")}>
+        <Pressable style={styles.signInButton} onPress={() =>  SignIn()}>
            <Text style={styles.signInButtonText}>Join Us</Text>
         </Pressable>
     )

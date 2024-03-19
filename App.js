f19474e8bb0ecf709735cf9bf8e2e61922005992
Luke Sanger <lukesngr@gmail.com>
2024-03-19 16:55:15 +1100

    Deep linking working now trying another thing

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from './pages/SplashScreen';
+import { Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
+export const linking = {
+  prefixes: ['boxoal://'],
+  config: {
+    initialRouteName: 'SplashScreen',
+    screens: {
+      Login: {
+        path: 'login/:accessToken'
+      }
+    },
+  }
+};
+
+
+export function Login({ navigation, route }) {
+  console.log(route);
+  const { accessToken } = route.params;
+  console.log(accessToken);
+  return <><Text>hello</Text></>
+}
+
 export default function App() {
-  return (<NavigationContainer>
+  return (<NavigationContainer linking={linking} fallback={<Text>Loading</Text>}>
       <Stack.Navigator>
         <Stack.Screen
           name="SplashScreen"
           component={SplashScreen}
           options={ {headerShown: false} }
         />
+        <Stack.Screen
+        name="Login"
+        component={Login}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );

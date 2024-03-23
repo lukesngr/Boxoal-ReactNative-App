67838cf060d0b8403c488424ed02401c64bfae2a
Luke Sanger <lukesngr@gmail.com>
2024-03-23 14:12:35 +1100

    Loading looking good time to add animation

--- components/Loading.js
+++ components/Loading.js
@@ -2,9 +2,9 @@ import {View, Text, Image} from 'react-native';
 
 export default function Loading() {
     return (
-        <View style={{display: 'flex', justifyContentCenter: 'center', alignItems: 'center', flexDirection: 'column'}}>
-            <Image source={require('../assets/icon2.png')} />
-            <Text style={{fontSize: 20}}>Loading...</Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+            <Image style={{width: 100, height: 100}} source={require('../assets/icon2.png')} />
+            <Text style={{fontSize: 20, color: 'black'}}>Loading...</Text>
         </View>
     )
 }
--- pages/Timeboxes.js
+++ pages/Timeboxes.js
@@ -1,10 +1,11 @@
 import { Text } from 'react-native';
 import TimeboxHeading from '../components/timeboxes/TimeboxHeading';
+import Loading from '../components/Loading';
 
 export default function Timeboxes() {
   return (
   <>
-    <TimeboxHeading />
+    <Loading />
   </>
   )
 }

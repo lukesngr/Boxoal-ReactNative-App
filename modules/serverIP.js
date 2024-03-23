2b6dd80d1319eeb56ab5d3a612019bdd6de138db
Luke Sanger <lukesngr@gmail.com>
2024-03-23 17:16:36 +1100

    got it sorta working now to test

---
+++ modules/serverIP.js
@@ -0,0 +1,2 @@
+let serverIP = "3.104.106.37:3000";
+export default serverIP;
--- pages/Timeboxes.js
+++ pages/Timeboxes.js
@@ -1,11 +1,37 @@
 import { Text } from 'react-native';
 import TimeboxHeading from '../components/timeboxes/TimeboxHeading';
 import Loading from '../components/Loading';
+import serverIP from '../modules/serverIP';
+import TimeboxGrid from '../components/timeboxes/TimeboxGrid';
 
 export default function Timeboxes() {
-  return (
-  <>
-    <Loading />
-  </>
-  )
+  const {selectedDate, setSelectedDate, ...leftovers} = useContext(ScheduleContext);
+  let startOfWeek = dayjs(selectedDate).startOf('week').hour(0).minute(0).toDate();
+  let endOfWeek = dayjs(selectedDate).endOf('week').add(1, 'day').hour(23).minute(59).toDate(); //another day as sometimes timeboxes will go into next week
+
+  AsyncStorage.getItem('username').then((value) => {
+    if(value !== null) {
+      const {status, data, error, refetch} = useQuery({
+        queryKey: ["schedules", selectedDate], 
+        queryFn: async () => {
+            const response = await axios.get(serverIP+"/api/getSchedules", { userEmail: props.session.user.email, startOfWeek, endOfWeek });
+        
+            return response;},
+        enabled: true})
+
+        if(status === "loading") {
+          return <Loading />
+      }
+    
+      return (
+      <>
+        <TimeboxHeading />
+        <TimeboxGrid data={data}></TimeboxGrid>
+      </>
+      )
+    }
+  })
+  
+  return <Text>Error contact developer</Text>;
+  
 }

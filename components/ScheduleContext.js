c8b9def79331cf6d4661d05727fb2281470c1144
Luke Sanger <lukesngr@gmail.com>
2024-03-23 12:52:45 +1100

    Datepicker chosen can't use dayjs format so will convert to dayjs format during calculation

--- components/ScheduleContext.js
+++ components/ScheduleContext.js
@@ -5,7 +5,7 @@ const ScheduleContext = createContext();
 
 function ScheduleContextProvider({ children }) {
     const [selectedSchedule, setSelectedSchedule] = useState("0");
-    const [selectedDate, setSelectedDate] = useState(dayjs());
+    const [selectedDate, setSelectedDate] = useState(new Date());
     
     return (
       <ScheduleContext.Provider value={{ selectedSchedule, setSelectedSchedule, expanded, setExpanded, selectedDate, setSelectedDate }}>
--- components/timeboxes/TimeboxHeading.js
+++ components/timeboxes/TimeboxHeading.js
@@ -1,9 +1,22 @@
 import { Text } from "react-native";
+import DatePicker from "react-native-date-picker";
 
 export default function TimeboxHeading(props) {
+    const [visible, setVisible] = useState(false);
+    const {selectedDate, setSelectedDate, ...leftovers} = useContext(ScheduleContext);
+
     return (
         <View>
             <Text>My Timeboxes</Text>
+            <FontAwesomeIcon onPress={setVisible(true)} icon={faCalendar} />
+            <DatePicker
+                modal
+                mode="date"
+                date={selectedDate}
+                onDateChange={setSelectedDate}
+                visible={visible}
+                onConfirm={() => setVisible(false)}
+                onCancel={() => setVisible(false)}></DatePicker>
         </View>
     )
 }

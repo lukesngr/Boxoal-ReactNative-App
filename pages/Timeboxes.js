44388c277efb7d363f5d7e34d5b0c2d464893601
Luke Sanger <lukesngr@gmail.com>
2024-03-25 14:59:55 +1100

    Looking good now moving onto the timebox grid

--- pages/Timeboxes.js
+++ pages/Timeboxes.js
@@ -3,40 +3,48 @@ import TimeboxHeading from '../components/timeboxes/TimeboxHeading';
 import Loading from '../components/Loading';
 import serverIP from '../modules/serverIP';
 import TimeboxGrid from '../components/timeboxes/TimeboxGrid';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ScheduleContext } from '../components/ScheduleContext';
 import dayjs from 'dayjs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useQuery } from '@tanstack/react-query';
 
+export const SessionContext = createContext();
+
 export default function Timeboxes() {
+  const [username, setUsername] = useState('');
   const {selectedDate, setSelectedDate, ...leftovers} = useContext(ScheduleContext);
   let startOfWeek = dayjs(selectedDate).startOf('week').hour(0).minute(0).toDate();
   let endOfWeek = dayjs(selectedDate).endOf('week').add(1, 'day').hour(23).minute(59).toDate(); //another day as sometimes timeboxes will go into next week
 
   AsyncStorage.getItem('username').then((value) => {
     if(value !== null) {
-      const {status, data, error, refetch} = useQuery({
-        queryKey: ["schedules", selectedDate], 
-        queryFn: async () => {
-            const response = await axios.get(serverIP+"/api/getSchedules", { userEmail: props.session.user.email, startOfWeek, endOfWeek });
-        
-            return response;},
-        enabled: true})
-
-        if(status === "loading") {
-          return <Loading />
-      }
-    
-      return (
-      <>
-        <TimeboxHeading />
-        <TimeboxGrid data={data}></TimeboxGrid>
-      </>
-      )
+      setUsernameSet(value);  
     }
   })
+
   
-  return <Text>Error contact developer</Text>;
+  if(username !== '') {
+    const {status, data, error, refetch} = useQuery({
+      queryKey: ["schedules", selectedDate], 
+      queryFn: async () => {
+          const response = await axios.get(serverIP+"/api/getSchedules", { userEmail: props.session.user.email, startOfWeek, endOfWeek });
+      
+          return response;},
+      enabled: true})
+
+      if(status === 'loading') return <Loading />
+
+      return (
+        <>
+          <TimeboxHeading />
+          <SessionContext.Provider value={{username}}>
+            <TimeboxGrid data={data}></TimeboxGrid>
+          </SessionContext.Provider>
+        </>
+        )
+  }
+
+  return <Loading />     
   
 }

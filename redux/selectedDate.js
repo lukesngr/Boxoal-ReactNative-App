d66b1a43c7e9d70e6453d64f11ec3e17cb160b52
Luke Sanger <lukesngr@gmail.com>
2024-04-02 19:00:01 +1100

    REmoved context and added as redux values

---
+++ redux/selectedDate.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const selectedDate = createSlice({
+  name: 'selectedDate',
+  initialState: {
+    value: new Date(),
+  },
+  reducers: {
+    set: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { set } = selectedDate.actions
+
+export default selectedDate.reducer

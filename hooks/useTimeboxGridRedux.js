c821edb840b57d560cb5769d275b24c74bf438fe
Luke Sanger <lukesngr@gmail.com>
2024-04-03 14:32:20 +1100

    Gotta fix some errors related to serializability but looking good so far

--- hooks/useTimeboxGridRedux.js
+++ hooks/useTimeboxGridRedux.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { generateTimeBoxGrid } from "@/modules/coreLogic";
+import { generateTimeBoxGrid } from "../modules/coreLogic";
 
 export default function useTimeboxGridRedux(schedule, selectedDate) {
     const dispatch = useDispatch();

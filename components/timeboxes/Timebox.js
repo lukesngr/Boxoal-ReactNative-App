2c1e74594dc8601e96a59fef695c5a11288ad9f3
Luke Sanger <lukesngr@gmail.com>
2024-04-11 16:12:29 +1000

    magic is starting to come together

--- components/timeboxes/Timebox.js
+++ components/timeboxes/Timebox.js
@@ -1,4 +1,5 @@
 import { Pressable, View, Text } from "react-native"
+import NormalTimebox from "./NormalTimebox";
 
 export default function Timebox(props) {
     function onPress() {
@@ -6,11 +7,29 @@ export default function Timebox(props) {
         console.log(props.time);
     }
 
+    const [modalVisible, setModalVisible] = useState(false);
+    const timeboxGrid = useSelector(state => state.timeboxGrid.value);
+    const dispatch = useDispatch();
+
+    let date = day.date+"/"+day.month;
+    let dayName = day.name;
+    let active = ifEqualOrBeyondCurrentDay(index, true, false)
+    let data = timeboxGrid.get(date)?.get(time);
+
     return (
     <View style={{borderWidth: 1, padding: 1, borderColor: 'black', width: 50.5, height: 30}}>
         <Pressable onPress={onPress}>
             <Text></Text>
         </Pressable>
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+            {data ? (<NormalTimebox></NormalTimebox>) : (<CreateTimeboxForm></CreateTimeboxForm>)}
+        </Modal>
     </View>
     )
 }

36233680229b58b6839dffd3a12dea72d9d5beaf
Luke Sanger <lukesngr@gmail.com>
2024-05-09 19:58:29 +1000

    Fixed timebox view

--- components/timeboxes/NormalTimebox.js
+++ components/timeboxes/NormalTimebox.js
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
 
 export default function NormalTimebox(props) {
-    let height = `calc(${props.data.numberOfBoxes*100}% + 2dp)`;
+    let height = 31*props.data.numberOfBoxes;
+
     return (
     <>
         <View style={{position: 'relative', height: height, backgroundColor: props.data.color, width: '100%'}}>
-            <Text>{props.data.title}</Text>
+            <Text style={{color: 'black'}}>{props.data.title}</Text>
         </View>
     </>
     )

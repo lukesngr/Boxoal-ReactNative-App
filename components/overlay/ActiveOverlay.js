9e04c2c7ead76b0b1ac1f2cba9c3ac4e8b91f137
Luke Sanger <lukesngr@gmail.com>
2024-05-12 20:51:27 +1000

    Don't know why I used Elevation

--- components/overlay/ActiveOverlay.js
+++ components/overlay/ActiveOverlay.js
@@ -5,5 +5,5 @@ export default function ActiveOverlay() {
     const overlayDimensions = useSelector(state => state.overlayDimensions.value);
     const activeOverlayHeight = useSelector(state => state.activeOverlayHeight.value);
     return <View style={{width: overlayDimensions[0], height: activeOverlayHeight, backgroundColor: '#D9D9D9', opacity: 1, 
-    zIndex: 999, elevation: 2, top: 0, position: 'absolute', transform: [{translateY: overlayDimensions[3]}]}}></View>
+    zIndex: 999, top: 0, position: 'absolute', transform: [{translateY: overlayDimensions[3]}]}}></View>
 }

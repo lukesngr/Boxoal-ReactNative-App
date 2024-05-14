5dc34ddd4cbe37399cc9a424bb86973a0a9b6ebc
Luke Sanger <lukesngr@gmail.com>
2024-05-14 19:21:54 +1000

    Calculations should now be working

--- modules/dateLogic.js
+++ modules/dateLogic.js
@@ -1,4 +1,6 @@
 import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import { convertToDateTime } from "./coreLogic";
 
 export function getArrayOfDayDateDayNameAndMonthForHeaders(todaysDate) {
     let dayToName = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
@@ -14,30 +16,38 @@ export function getArrayOfDayDateDayNameAndMonthForHeaders(todaysDate) {
 
 
 export function ifCurrentDay(number, returnIfTrue, returnIfFalse) {
+    const {wakeupTime} = useSelector(state => state.scheduleEssentials.value);
     const dateObject = new Date();
     let currentDay = dateObject.getDay();
+    let cutOffDateTime = convertToDateTime(wakeupTime, dateObject.getDay()+'/'+dateObject.getMonth());
 
     if(typeof number !== 'number') { 
         console.log("Non-number datatype given to comparison function");
         return returnIfFalse;
     }
 
-    if(number == currentDay) {
+    if(number == currentDay && dateObject >= cutOffDateTime) {
+        return returnIfTrue;
+    }else if(number == currentDay+1 && dateObject < cutOffDateTime) {
         return returnIfTrue;
     }
     return returnIfFalse;
 }
 
 export function ifEqualOrBeyondCurrentDay(number, returnIfTrue, returnIfFalse) {
+    const {wakeupTime} = useSelector(state => state.scheduleEssentials.value);
     const dateObject = new Date();
     let currentDay = dateObject.getDay();
+    let cutOffDateTime = convertToDateTime(wakeupTime, dateObject.getDay()+'/'+dateObject.getMonth());
 
     if(typeof number !== 'number') { 
         console.log("Non-number datatype given to comparison function");
         return returnIfFalse;
     }
     
-    if(number >= currentDay) {
+    if(number >= currentDay && dateObject >= cutOffDateTime) {
+        return returnIfTrue;
+    }else if(number >= currentDay+1 && dateObject < cutOffDateTime) {
         return returnIfTrue;
     }
     return returnIfFalse;

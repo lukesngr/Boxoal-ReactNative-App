bba22b10759b7bfcf3fb3f7444721031905fce68
Luke Sanger <lukesngr@gmail.com>
2024-05-17 17:13:52 +1000

    Fixing bug

--- components/overlay/Overlay.js
+++ components/overlay/Overlay.js
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 import { View } from "react-native";
 
-export default function Overlay(props) {
+export default function Overlay() {
     const overlayDimensions = useSelector(state => state.overlayDimensions.value);
     
-    return (<>{!props.notActive && <View style={{width: overlayDimensions[0], 
+    return (<View style={{width: overlayDimensions[0], 
         height: overlayDimensions[1], 
         backgroundColor: '#D9D9D9', opacity: 1, 
-        zIndex: 998, elevation: 1, top: 0, position: 'absolute', transform: [{translateY: overlayDimensions[3]}]}}></View>}</>);
+        zIndex: 998, elevation: 1, top: 0, position: 'absolute', transform: [{translateY: overlayDimensions[3]}]}}></View>});
 }

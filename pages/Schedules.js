0b170a291ca0fc42b272a8dcfba6fd908789562b
Luke Sanger <lukesngr@gmail.com>
2024-05-21 15:02:28 +1000

    Trying to figure out how to do this

--- pages/Schedules.js
+++ pages/Schedules.js
@@ -1,5 +1,8 @@
 export default function Schedules() {
     return (
-        <></>
+        <>
+            <Text style={{fontSize: 28, color: 'black', textAlign: 'center', marginTop: 4}}>My Schedules</Text>
+            
+        </>
     )
 }

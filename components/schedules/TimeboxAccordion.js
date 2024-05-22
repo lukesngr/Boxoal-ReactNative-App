d1da13551532922bfdd755b5868009f8102e6f98
Luke Sanger <lukesngr@gmail.com>
2024-05-22 16:48:19 +1000

    Should be working now

--- components/schedules/TimeboxAccordion.js
+++ components/schedules/TimeboxAccordion.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
 import { Pressable } from "react-native";
 import { Text, View } from "react-native";
 import { useState } from "react";
+import { convertToTimeAndDate } from "../../modules/coreLogic";
 
 const styles = {
     modalContainer: {
@@ -15,32 +16,25 @@ const styles = {
 
 export default function TimeboxAccordion(props) {
 
+    const [modalVisible, setModalVisible] = useState(false);
+    let data = props.timebox;
+    let [time, date] = convertToTimeAndDate(props.timebox.startTime);
+
     function onPress() {
         setModalVisible(true);
-        const timeboxGrid = useSelector(state => state.timeboxGrid.value);
-    let date = props.day.date+"/"+props.day.month;
-    let data;
-
-    if(timeboxGrid) { 
-        if(timeboxGrid[date]) {
-            data = timeboxGrid[date][props.time];
-        }
-    }
     }
 
-    const [modalVisible, setModalVisible] = useState(false);
-    
-
-    console.log(props.timebox);
     return (
         <View style={{backgroundColor: 'white', padding: 10, margin: 10}}>
             <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 <Text style={{color: 'black', fontSize: 20, flexShrink: 1}}>{props.timebox.title}</Text>
-                <FontAwesomeIcon icon={faGear} size={30} color="black" />
+                <Pressable onPress={onPress}>
+                    <FontAwesomeIcon icon={faGear} size={30} color="black" />
+                </Pressable>
             </View>
             <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => {setModalVisible(!modalVisible);}}>
                 <View style={styles.modalContainer}>
-                    <TimeboxActionsForm time={props.time} date={date} data={data} close={setModalVisible}></TimeboxActionsForm>
+                    <TimeboxActionsForm time={time} date={date} data={data} close={setModalVisible}></TimeboxActionsForm>
                 </View>
             </Modal>
         </View>

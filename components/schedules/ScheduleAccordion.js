bda90f789c84531b6366e18edab342328c0d1865
Luke Sanger <lukesngr@gmail.com>
2024-05-22 16:07:30 +1000

    Fixing bugs with accordions

--- components/schedules/ScheduleAccordion.js
+++ components/schedules/ScheduleAccordion.js
@@ -3,6 +3,6 @@ import ScheduleItem from "./ScheduleItem"
 export default function ScheduleAccordion(props) {
 
     return props.data.map((schedule, index) => {
-        <ScheduleItem key={index} schedule={schedule}></ScheduleItem>
+        return <ScheduleItem key={index} schedule={schedule}></ScheduleItem>
     })
 }
--- components/schedules/ScheduleItem.js
+++ components/schedules/ScheduleItem.js
@@ -7,18 +7,19 @@ import { useState } from "react";
 
 export default function ScheduleItem(props) {
     const [accordionOpen, setAccordionOpen] = useState(false);
+
     return (
-    <Pressable onPress={() => setAccordionOpen(!accordionOpen)}>
-        <View key={index} style={{backgroundColor: 'white', padding: 10, margin: 10}}>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={{color: 'black', fontSize: 25}}>{props.schedule.name}</Text>
-                <View style={{flexDirection: 'row'}}>
-                    <FontAwesomeIcon icon={faGear} size={30} color="black" />
-                    <FontAwesomeIcon icon={accordionOpen ? faChevronDown : faChevronUp} size={30} color="black" />
+        <Pressable onPress={() => setAccordionOpen(!accordionOpen)}>
+            <View style={{backgroundColor: 'white', padding: 10, margin: 10}}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Text style={{color: 'black', fontSize: 25}}>{props.schedule.name}</Text>
+                    <View style={{flexDirection: 'row'}}>
+                        <FontAwesomeIcon icon={faGear} size={30} color="black" />
+                        <FontAwesomeIcon icon={accordionOpen ? faChevronDown : faChevronUp} size={30} color="black" />
+                    </View>
                 </View>
+                {accordionOpen && <GoalAccordion data={schedule.goals}></GoalAccordion>}
             </View>
-            {accordionOpen && <GoalAccordion data={schedule.goals}></GoalAccordion>}
-        </View>
-    </Pressable>
+        </Pressable>
     )
 }

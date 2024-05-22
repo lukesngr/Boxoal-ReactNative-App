60aee2a12be5337a82739ad3ebf86c893338a1ba
Luke Sanger <lukesngr@gmail.com>
2024-05-22 15:53:30 +1000

    Made goal accordion working

--- components/schedules/GoalAccordion.js
+++ components/schedules/GoalAccordion.js
@@ -1,3 +1,20 @@
-export default function GoalAccordion() {
-    return <></>
+export default function GoalAccordion(props) {
+    const [accordionOpen, setAccordionOpen] = useState(false);
+
+    return props.data.map((goal, index) => {
+        return (
+            <Pressable onPress={() => setAccordionOpen(!accordionOpen)}>
+                <View key={index} style={{backgroundColor: 'white', padding: 10, margin: 10}}>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <Text style={{color: 'black', fontSize: 25}}>{goal.name}</Text>
+                        <View style={{flexDirection: 'row'}}>
+                            <FontAwesomeIcon icon={faGear} size={30} color="black" />
+                            <FontAwesomeIcon icon={accordionOpen ? faChevronDown : faChevronUp} size={30} color="black" />
+                        </View>
+                    </View>
+                    {accordionOpen && <></>}
+                </View>
+            </Pressable>
+        )
+    })
 }

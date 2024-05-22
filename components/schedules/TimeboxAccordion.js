4ef151cbd3833afcbdcca2706bdac2f3468661a9
Luke Sanger <lukesngr@gmail.com>
2024-05-22 16:19:14 +1000

    Should be working now

--- components/schedules/TimeboxAccordion.js
+++ components/schedules/TimeboxAccordion.js
@@ -3,24 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
 import { Pressable } from "react-native";
 import { Text, View } from "react-native";
 import { useState } from "react";
-import TimeboxAccordion from "./TimeboxAccordion";
 
-export default function GoalAccordion(props) {
+export default function TimeboxAccordion(props) {
     const [accordionOpen, setAccordionOpen] = useState(false);
 
     return (
         <Pressable onPress={() => setAccordionOpen(!accordionOpen)}>
             <View style={{backgroundColor: 'white', padding: 10, margin: 10}}>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <Text style={{color: 'black', fontSize: 20, flexShrink: 1}}>{props.goal.name}</Text>
+                    <Text style={{color: 'black', fontSize: 20, flexShrink: 1}}>{props.timebox.title}</Text>
                     <View style={{flexDirection: 'row'}}>
                         <FontAwesomeIcon icon={faGear} size={30} color="black" />
                         <FontAwesomeIcon icon={accordionOpen ? faChevronDown : faChevronUp} size={30} color="black" />
                     </View>
                 </View>
-                {accordionOpen && props.goal.timeboxes.map((timebox, index) => {
-                    return <TimeboxAccordion key={index} timebox={timebox}></TimeboxAccordion>
-                })}
+                {accordionOpen && <></>}
             </View>
         </Pressable>
     )

4df1b91200f91ca459cd23388c0e0f3adb7b3bc6
Luke Sanger <lukesngr@gmail.com>
2024-05-24 17:23:52 +1000

    Should be working now

--- components/goals/EditGoalForm.js
+++ components/goals/EditGoalForm.js
@@ -56,12 +56,14 @@ const styles = StyleSheet.create({
 export default function EditGoalForm(props) {
     const [name, setName] = useState(props.data.name);
     const [priority, setPriority] = useState(""+props.data.prioirty);
-    const [targetDate, setTargetDate] = useState(new Date(props.goal.targetDate).toISOString().slice(0, 19));
+    const [targetDate, setTargetDate] = useState(new Date(props.goal.targetDate));
+    const [visible, setVisible] = useState(false);
 
     function updateTimeBox() {
-        axios.put(serverIP+'/updateTimeBox', {
-            title,
-            description, 
+        axios.put(serverIP+'/updateGoal', {
+            name,
+            priority: parseInt(priority), //damn thing won't convert auto even with number input
+            targetDate: targetDate.toUTCString(), 
             id: props.data.id
         },
         {headers: { 'Origin': 'http://localhost:3000' }}
@@ -74,14 +76,14 @@ export default function EditGoalForm(props) {
         })
     }
     
-    function deleteTimeBox() {
+    function deleteGoal() {
         
-        axios.post(serverIP+'/deleteTimebox', {
+        axios.post(serverIP+'/deleteGoal', {
             id: props.data.id
         },
         {headers: { 'Origin': 'http://localhost:3000' }}
         ).then(async () => {   
-            Alert.alert("Deleted timebox!");
+            Alert.alert("Deleted goal!");
             await queryClient.refetchQueries();
         }).catch(function(error) {
             Alert.alert("Error occurred please try again or contact developer");
@@ -90,18 +92,26 @@ export default function EditGoalForm(props) {
     }
 
     return (
-    <View style={styles.overallModal}>
-            <View style={styles.titleBarContainer}>  
-                <Text style={styles.title}>Edit Goal</Text>
-                <Pressable onPress={props.close}>
-                    <FontAwesomeIcon icon={faXmark} size={25}/>
+    <>
+        <View style={styles.overallModal}>
+                <View style={styles.titleBarContainer}>  
+                    <Text style={styles.title}>Edit Goal</Text>
+                    <Pressable onPress={props.close}>
+                        <FontAwesomeIcon icon={faXmark} size={25}/>
+                    </Pressable>
+                </View>
+                <Text style={styles.label}>Name</Text>
+                <TextInput style={styles.textInput} onChangeText={setName} value={name}></TextInput>
+                <Text style={styles.label}>Priority</Text>
+                <TextInput style={styles.textInput} keyboardType="numeric" onChangeText={setPriority} value={priority}></TextInput>
+                <Text style={styles.label}>Target Date</Text>
+                <Pressable onPress={() => setVisible(true)}>
+                    <FontAwesomeIcon icon={faCalendar} size={20}/>
                 </Pressable>
-            </View>
-            <Text style={styles.label}>Name</Text>
-            <TextInput style={styles.textInput} onChangeText={setName} value={name}></TextInput>
-            <Text style={styles.label}>Priority</Text>
-            <TextInput style={styles.textInput} keyboardType="numeric" onChangeText={setPriority} value={priority}></TextInput>
-            <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Delete" onPress={deleteTimeBox} />
-            <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Update" onPress={updateTimeBox} />
-    </View>)
+                <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Delete" onPress={deleteGoal} />
+                <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Update" onPress={updateTimeBox} />
+        </View>
+        <DatePicker modal mode="date" date={targetDate} onDateChange={(date) => setTargetDate(date)} open={visible} 
+        onConfirm={(date) => { setTargetDate(date); setVisible(false); }} onCancel={() => setVisible(false)}></DatePicker>
+    </>)
 }

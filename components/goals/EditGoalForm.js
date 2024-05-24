f5c877797a1d36b5ac98d0eacb63a32b3955b5fa
Luke Sanger <lukesngr@gmail.com>
2024-05-24 17:00:05 +1000

    Need to add datepicker and change methods than good to go

---
+++ components/goals/EditGoalForm.js
@@ -0,0 +1,107 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { StyleSheet, View, Text, Pressable, TextInput } from "react-native";
+import axios from "axios";
+import { useState } from "react";
+import { Alert } from "react-native";
+import serverIP from "../../modules/serverIP";
+import Button from "./Button";
+import { queryClient } from "../../App";
+
+const styles = StyleSheet.create({
+    overallModal: {
+        backgroundColor: 'white',
+        padding: 10,
+        width: '80%',
+        height: 'auto',
+    },
+    title: {
+        color: 'black',
+        fontSize: 22,
+        padding: 0,
+        margin: 0,
+    },
+    label: {
+        color: 'black',
+        fontSize: 20,
+    },
+    textInput: {
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 1,
+        fontSize: 20,
+    },
+    buttonOutlineStyle: {
+        backgroundColor: '#7FFFD4',
+        padding: 5,
+        marginTop: 10,
+    },
+    buttonTextStyle: {
+        color: 'black',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    titleBarContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 'auto',
+        margin: 0,
+        padding: 0,
+    },
+});
+
+
+
+export default function EditGoalForm(props) {
+    const [name, setName] = useState(props.data.name);
+    const [priority, setPriority] = useState(""+props.data.prioirty);
+    const [targetDate, setTargetDate] = useState(new Date(props.goal.targetDate).toISOString().slice(0, 19));
+
+    function updateTimeBox() {
+        axios.put(serverIP+'/updateTimeBox', {
+            title,
+            description, 
+            id: props.data.id
+        },
+        {headers: { 'Origin': 'http://localhost:3000' }}
+        ).then(async () => {
+            Alert.alert("Updated timebox!");
+            await queryClient.refetchQueries();
+        }).catch(function(error) {
+            Alert.alert("Error occurred please try again or contact developer");
+            console.log(error); 
+        })
+    }
+    
+    function deleteTimeBox() {
+        
+        axios.post(serverIP+'/deleteTimebox', {
+            id: props.data.id
+        },
+        {headers: { 'Origin': 'http://localhost:3000' }}
+        ).then(async () => {   
+            Alert.alert("Deleted timebox!");
+            await queryClient.refetchQueries();
+        }).catch(function(error) {
+            Alert.alert("Error occurred please try again or contact developer");
+            console.log(error); 
+        });
+    }
+
+    return (
+    <View style={styles.overallModal}>
+            <View style={styles.titleBarContainer}>  
+                <Text style={styles.title}>Edit Goal</Text>
+                <Pressable onPress={props.close}>
+                    <FontAwesomeIcon icon={faXmark} size={25}/>
+                </Pressable>
+            </View>
+            <Text style={styles.label}>Name</Text>
+            <TextInput style={styles.textInput} onChangeText={setName} value={name}></TextInput>
+            <Text style={styles.label}>Priority</Text>
+            <TextInput style={styles.textInput} keyboardType="numeric" onChangeText={setPriority} value={priority}></TextInput>
+            <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Delete" onPress={deleteTimeBox} />
+            <Button textStyle={styles.buttonTextStyle} outlineStyle={styles.buttonOutlineStyle} title="Update" onPress={updateTimeBox} />
+    </View>)
+}

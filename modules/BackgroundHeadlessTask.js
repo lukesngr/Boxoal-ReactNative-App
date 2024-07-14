6c0c3eb89ed56d343fdd4ec6d6e1b2aefcd08a71
Luke Sanger <lukesngr@gmail.com>
2024-07-14 20:30:07 +1000

    Hopefully this is a better solution

--- modules/BackgroundHeadlessTask.js
+++ modules/BackgroundHeadlessTask.js
@@ -2,7 +2,13 @@ const { updateNotification } = require("./coreLogic");
 
 module.exports = async (taskData) => {
     let {timebox, schedule, recordingStartTime} = taskData;
-    updateNotification(timebox, schedule, recordingStartTime)
-    setInterval(() => updateNotification(timebox, schedule, recordingStartTime), 2000);
+    
+    function updateThenWait() {
+        updateNotification(timebox, schedule, recordingStartTime)
+        setInterval(updateThenWait, 2000);
+    }
+
+    updateThenWait();
+    
     return Promise.resolve();
 };

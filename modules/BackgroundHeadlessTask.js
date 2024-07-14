3f9aab540413cb225c94be6b8b223b974633db41
Luke Sanger <lukesngr@gmail.com>
2024-07-14 14:57:28 +1000

    Made periodic minutes to 16 and iterations to 2 so there's no gap in the update

--- modules/BackgroundHeadlessTask.js
+++ modules/BackgroundHeadlessTask.js
@@ -3,7 +3,7 @@ const { updateNotification } = require("./coreLogic");
 module.exports = async (taskData) => {
     let {timebox, schedule, recordingStartTime} = taskData;
     updateNotification(timebox, schedule, recordingStartTime)
-    for(let i = 1; i < 8; i++) {
+    for(let i = 1; i < 9; i++) {
         setTimeout(() => updateNotification(timebox, schedule, recordingStartTime), 2000*i);
     }
     return Promise.resolve();

d800a3daa18ea8ae391ec5296bc0adab66ca243a
Luke Sanger <lukesngr@gmail.com>
2024-07-14 20:30:34 +1000

    Forgot to change to set timeout

--- modules/BackgroundHeadlessTask.js
+++ modules/BackgroundHeadlessTask.js
@@ -2,10 +2,10 @@ const { updateNotification } = require("./coreLogic");
 
 module.exports = async (taskData) => {
     let {timebox, schedule, recordingStartTime} = taskData;
-    
+
     function updateThenWait() {
         updateNotification(timebox, schedule, recordingStartTime)
-        setInterval(updateThenWait, 2000);
+        setTimeout(updateThenWait, 2000);
     }
 
     updateThenWait();

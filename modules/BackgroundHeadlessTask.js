12a610036ea79ca71d00ef7a4f0d893fa2674b8e
Luke Sanger <lukesngr@gmail.com>
2024-07-14 14:43:59 +1000

    Made a logic error in my code

--- modules/BackgroundHeadlessTask.js
+++ modules/BackgroundHeadlessTask.js
@@ -3,8 +3,8 @@ const { updateNotification } = require("./coreLogic");
 module.exports = async (taskData) => {
     let {timebox, schedule, recordingStartTime} = taskData;
     updateNotification(timebox, schedule, recordingStartTime)
-    for(let i = 0; i < 6; i++) {
-        setTimeout(updateNotification(timebox, schedule, recordingStartTime), 2000);
+    for(let i = 1; i < 6; i++) {
+        setTimeout(() => updateNotification(timebox, schedule, recordingStartTime), 2000*i);
     }
     return Promise.resolve();
 };

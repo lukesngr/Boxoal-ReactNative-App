dbcfab81c14108f9a81e279a7d76bd3adeea0f9b
Luke Sanger <lukesngr@gmail.com>
2024-07-15 16:24:25 +1000

    No need to put data in the redux cause passed data through intents

--- redux/timeboxRecording.js
+++ redux/timeboxRecording.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const timeboxRecording = createSlice({
   name: 'timeboxRecording',
   initialState: {
-    value: {timebox: {id: -1}, timeboxDate: 0, recordingStartTime: -1},
+    value: {timeboxID: -1, timeboxDate: 0, recordingStartTime: -1},
   },
   reducers: {
     set: (state, action) => {

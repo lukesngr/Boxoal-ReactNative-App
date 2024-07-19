f1d46ab4a38283f64d7c59d4999c2bb8a3995636
Luke Sanger <lukesngr@gmail.com>
2024-07-19 15:29:45 +1000

    need to convert to integer

--- modules/coreLogic.js
+++ modules/coreLogic.js
@@ -254,8 +254,10 @@ export async function initialNotificationSetup() {
 }
 
 export function recordIfNotificationPressed(dispatch, routeParams) {
-    if(routeParams.hasOwn('timeboxID')) {
+    if(Object.hasOwn(routeParams, 'timeboxID')) {
         const { timeboxID, scheduleID, recordingStartTime } = routeParams;
+        timeboxID = parseInt(timeboxID);
+        scheduleID = parseInt(scheduleID);
         dispatch({type: 'timeboxRecording/set', payload: {timeboxID: -1, timeboxDate: 0, recordingStartTime: 0}});
         dispatch(setActiveOverlayInterval());
         axios.post(serverIP+'/createRecordedTimebox', 
@@ -273,7 +275,7 @@ export function recordIfNotificationPressed(dispatch, routeParams) {
 }
 
 export function setUserNameUsingGithubAccessCode(dispatch, routeParams) {
-    if(routeParams.hasOwn('accessToken')) {
+    if(Object.hasOwn(routeParams, 'accessToken')) {
         const { accessToken } = routeParams;
         axios.get('https://api.github.com/user/emails', {
         headers: {

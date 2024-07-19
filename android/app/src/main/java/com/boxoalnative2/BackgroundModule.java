ed360321ecc33172c50d8557e15ef2c812ddeb9d
Luke Sanger <lukesngr@gmail.com>
2024-07-19 15:57:50 +1000

    Changed to onetimeworkrequest no need for periodic

--- android/app/src/main/java/com/boxoalnative2/BackgroundModule.java
+++ android/app/src/main/java/com/boxoalnative2/BackgroundModule.java
@@ -11,9 +11,7 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import androidx.work.Data;
-
-import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.PeriodicWorkRequest;
+import androidx.work.ExistingWorkPolicy;
 import android.util.Log;
 import androidx.work.OneTimeWorkRequest;
 
@@ -21,7 +19,7 @@ public class BackgroundModule extends ReactContextBaseJavaModule {
     private static final String MODULE_NAME = "BackgroundWorkManager";
 
     private Context mContext;
-    private PeriodicWorkRequest workRequest;
+    private OneTimeWorkRequest workRequest;
 
     BackgroundModule(@Nonnull ReactApplicationContext reactContext) {
         super(reactContext);
@@ -31,8 +29,8 @@ public class BackgroundModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void startBackgroundWork(String timebox, String schedule, String recordingStartTime) {
         Data serviceInput = new Data.Builder().putString("timebox", timebox).putString("schedule", schedule).putString("recordingStartTime", recordingStartTime).build();
-        workRequest = new PeriodicWorkRequest.Builder(BackgroundWorker.class, 16, TimeUnit.MINUTES).setInputData(serviceInput).build();
-        WorkManager.getInstance(mContext).enqueueUniquePeriodicWork("recordingUpdate", ExistingPeriodicWorkPolicy.REPLACE, workRequest);
+        workRequest = new OneTimeWorkRequest.Builder(BackgroundWorker.class).setInputData(serviceInput).build();
+        WorkManager.getInstance(mContext).enqueueUniqueWork("recordingUpdate", ExistingWorkPolicy.REPLACE, workRequest);
     }
 
     @ReactMethod

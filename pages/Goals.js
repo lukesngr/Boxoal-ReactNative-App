bbe70edbb31ffef2ff78165f067cf67b273c8ab1
Luke Sanger <lukesngr@gmail.com>
2024-07-31 00:30:12 +1000

    Added more space between icons

--- pages/Goals.js
+++ pages/Goals.js
@@ -1,27 +1,30 @@
-import dayjs from "dayjs";
-import { useSelector } from "react-redux";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import ScheduleAccordion from "../components/schedules/ScheduleAccordion";
 import { Text } from "react-native";
-import Loading from "../components/Loading";
 import { View } from "react-native";
 import { IconButton } from "react-native-paper";
+import CreateScheduleForm from "../components/schedules/CreateScheduleForm";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function Goals(props) {
+    const {selectedSchedule} = useSelector(state => state.settings.value);
+    const [createScheduleVisible, setCreateScheduleVisible] = useState(false);
+    let schedule = props.data[Number(selectedSchedule)];
 
-    return (
+    return (<>
         <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%', padding: 20, paddingLeft: 15, paddingRight: 15}}>
             <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
                 <Text style={{fontFamily: 'KameronRegular', fontSize: 35, color: 'black', textAlign: 'left', marginTop: 10, paddingLeft: 40}}>Goals</Text>
-                <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 30}}> 
-                    <Text style={{fontSize: 25, color: 'black', textAlign: 'left', marginTop: 5, marginRight: 80}}> Timeboxes</Text>
+                <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 40}}> 
+                    <Text style={{fontSize: 25, color: 'black', textAlign: 'left', marginTop: 5, marginRight: 180}}>{schedule.title}</Text>
                     <View style={{flexDirection: 'row', justifyContent: 'space-between', gap: -25}}>
                         <IconButton icon="cog" size={36}></IconButton>
-                        <IconButton icon="plus" size={36}></IconButton>
+                        <IconButton icon="plus" size={36} onPress={() => setCreateScheduleVisible(true)}></IconButton>
                     </View>
                 </View>
             </View>
+            
         </View>
+        <CreateScheduleForm visible={createScheduleVisible} close={() => setCreateScheduleVisible(false)}></CreateScheduleForm>
+        </>
     )
 }

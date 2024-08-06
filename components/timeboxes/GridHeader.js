737739bc5071661a5572343ae337b050404f2f10
Luke Sanger <lukesngr@gmail.com>
2024-08-06 15:02:30 +1000

    Header should be working now, can't believe react interpreted functions as hooks

--- components/timeboxes/GridHeader.js
+++ components/timeboxes/GridHeader.js
@@ -4,44 +4,48 @@ import RecordingOverlay from "../overlay/RecordingOverlay";
 import { ifCurrentDay, ifEqualOrBeyondCurrentDay } from "../../modules/dateLogic";
 import Overlay from "../overlay/Overlay";
 import { getCurrentDay } from "../../modules/dateLogic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import useOverlayDimensions from "../../hooks/useOverlayDimensions";
 import { styles } from "../../styles/styles";
+import { set } from "../../redux/activeOverlayInterval";
 
 export default function GridHeader(props) {
     const [headerHeight, setHeaderHeight] = useState(0);
     const [headerWidth, setHeaderWidth] = useState(0);
-    useOverlayDimensions(headerHeight, headerWidth); 
-    let dayToName = props.dayToName;
-    let currentDay = dayToName[getCurrentDay()];
+    const [dayToName, setDayToName] = useState(props.dayToName);
+    const [headerFontsize, setHeaderFontsize] = useState(16);
     const onDayView = useSelector(state => state.onDayView.value);
+    let currentDay = getCurrentDay();
+    useOverlayDimensions(headerHeight, headerWidth);
 
-    return (
-    <>
-        <View style={styles.timeboxCell}></View>
-        {onDayView && 
-            <View key={currentDay.day} style={{backgroundColor: 'black', ...styles.timeboxCell}} 
-            onLayout={(event) => {setHeaderHeight(event.nativeEvent.layout.height); setHeaderWidth(event.nativeEvent.layout.width);}}>
-                <Text style={{fontSize: 25, color: 'white'}}>{currentDay.name+" ("+currentDay.date+"/"+currentDay.month+")"}</Text>
-                <ActiveOverlay></ActiveOverlay>
-                <Overlay></Overlay>
-                <RecordingOverlay day={currentDay}></RecordingOverlay>
-            </View>
+    useEffect(() => {
+        if(onDayView) {
+            setDayToName([dayToName[currentDay]]); 
+            setHeaderFontsize(18);
+        }else{
+            setDayToName(props.dayToName);
+            setHeaderFontsize(16);
         }
-        {!onDayView && dayToName.map((day, index) => 
-            (<View key={day.day} style={{backgroundColor: ifCurrentDay(day.day, 'black', 'white'), ...styles.timeboxCell}}
+    }, [onDayView])
+    
+
+    return (<>
+        <View style={styles.timeboxCell}></View>
+        {dayToName.map((day, index) => {
+            return (<>
+            <View key={day.day} style={{backgroundColor: day.day == currentDay ? 'black' :  'white', ...styles.timeboxCell}}
                         onLayout={(event) => {
                             if(index == 0) {
                                 setHeaderHeight(event.nativeEvent.layout.height);
                                 setHeaderWidth(event.nativeEvent.layout.width);
                             }
                         }}>
-                <Text style={{fontSize: 16, color: ifCurrentDay(day.day, 'white', 'black')}}>{day.name+" ("+day.date+"/"+day.month+")"}</Text>
-                {ifCurrentDay(day.day, true, false) && <ActiveOverlay></ActiveOverlay>}
-                {ifEqualOrBeyondCurrentDay(day.day, false, true) && <Overlay></Overlay>}
+                <Text style={{fontSize: 16, color: day.day == currentDay ? 'black' :  'white'}}>{day.name+" ("+day.date+"/"+day.month+")"}</Text>
+                {day.day == currentDay && <ActiveOverlay></ActiveOverlay>}
+                {day.day < currentDay && <Overlay></Overlay>}
                 <RecordingOverlay day={day}></RecordingOverlay>
-            </View>)
-        )}
+            </View></>)
+        })}
     </>)
 }

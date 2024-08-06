3256f6b65eb60266bca87701db97e9a63ced19f5
Luke Sanger <lukesngr@gmail.com>
2024-08-06 11:15:01 +1000

    Conditional works in this case cause no hooks rendered

--- components/timeboxes/NormalTimebox.js
+++ components/timeboxes/NormalTimebox.js
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 export default function NormalTimebox(props) {
     const onDayView = useSelector(state => state.onDayView.value);
     let timeboxHeight = 31;
-    if(onDayView) {
-        timeboxHeight = 60;
-    }
+    if(onDayView) { timeboxHeight = 60; }
     let height = timeboxHeight*props.data.numberOfBoxes;
 
     return (

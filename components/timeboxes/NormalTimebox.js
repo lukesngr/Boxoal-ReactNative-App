d0ab733aa12fbd7009290c6a5c81d31ccba34af9
Luke Sanger <lukesngr@gmail.com>
2024-08-06 18:51:27 +1000

    removed state cause don't thik I need it

--- components/timeboxes/NormalTimebox.js
+++ components/timeboxes/NormalTimebox.js
@@ -1,15 +1,18 @@
 import { Text, View } from "react-native";
+import { styles } from "../../styles/styles";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function NormalTimebox(props) {
     const onDayView = useSelector(state => state.onDayView.value);
-    let timeboxHeight = 31;
-    if(onDayView) { timeboxHeight = 60; }
+    let fontSize = onDayView ? 20 : 12;
+    let timeboxHeight = onDayView ? styles.enlargedTimeboxHeight : styles.normalTimeboxHeight;
     let height = timeboxHeight*props.data.numberOfBoxes;
 
     return (
     <>
         <View style={{position: 'relative', height: height, backgroundColor: props.data.color, width: '100%'}}>
-            <Text style={{color: 'black'}}>{props.data.title}</Text>
+            <Text style={{fontSize: fontSize, color: 'black'}}>{props.data.title}</Text>
         </View>
     </>
     )

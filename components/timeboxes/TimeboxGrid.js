43dd0c606f8cb9addb5f9e9883680644eab2da45
Luke Sanger <lukesngr@gmail.com>
2024-08-06 17:46:43 +1000

    currentDay moved up

--- components/timeboxes/TimeboxGrid.js
+++ components/timeboxes/TimeboxGrid.js
@@ -1,21 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";
-import { getArrayOfDayDateDayNameAndMonthForHeaders, getCurrentDay, ifEqualOrBeyondCurrentDay } from "../../modules/dateLogic";
+import { useSelector } from "react-redux";
+import { getArrayOfDayDateDayNameAndMonthForHeaders, getCurrentDay } from "../../modules/dateLogic";
 import { returnTimesSeperatedForSchedule } from "../../modules/timeLogic";
 import useTimeboxGridRedux from "../../hooks/useTimeboxGridRedux";
 import { useScheduleSetter } from "../../hooks/useScheduleSetter";
-import { View, Text, ScrollView } from "react-native";
-import Timebox from "./Timebox";
-import { ifCurrentDay } from "../../modules/dateLogic";
-import { useState } from "react";
-import useOverlayDimensions from "../../hooks/useOverlayDimensions";
-import Overlay from "../overlay/Overlay";
-import ActiveOverlay from "../overlay/ActiveOverlay";
+import { View, ScrollView } from "react-native";
 import useActiveOverlay from "../../hooks/useActiveOverlay";
-import RecordingOverlay from "../overlay/RecordingOverlay";
 import RecordedTimeBoxOverlay from "../overlay/RecordedTimeBoxOverlay";
 import { styles } from "../../styles/styles";
 import GridHeader from "./GridHeader";
 import GridBody from "./GridBody";
+import { useDaySelected } from "../../hooks/useDaySelected";
 
 
 export default function TimeboxGrid(props) {
@@ -24,18 +18,21 @@ export default function TimeboxGrid(props) {
     const schedule = props.data[selectedSchedule];
     const dayToName = getArrayOfDayDateDayNameAndMonthForHeaders(selectedDate); //get all info to make headers look nice
     const listOfTimes = returnTimesSeperatedForSchedule(schedule); //get times that go down each row
+    let currentDay = getCurrentDay();
     useTimeboxGridRedux(schedule, selectedDate); //make a map for the timeboxes with another map inside it, makes lookup fast
     useScheduleSetter(schedule); //set schedule data to redux store (timeboxes, recordedTimeboxes, goals
     useActiveOverlay(schedule);
+    useDaySelected(currentDay);
+
     return (
     <ScrollView>
         <View style={styles.overallView}>
             <View style={{flexDirection: 'row'}}>
-                <GridHeader dayToName={dayToName}></GridHeader>
+                <GridHeader currentDay={currentDay} dayToName={dayToName}></GridHeader>
             </View>
             <View style={{flexDirection: 'column'}}>
                 {listOfTimes.map((time, index) => {
-                    return <GridBody key={index} time={time} dayToName={dayToName}></GridBody>
+                    return <GridBody key={index} currentDay={currentDay} time={time} dayToName={dayToName}></GridBody>
                 })}
             </View>
             <View style={{position: 'absolute', transform: [{translateX: 50}], zIndex: 999}}>

8085cf1c64dc2c31165bfed6012ae8a7b3217094
Luke Sanger <lukesngr@gmail.com>
2024-08-07 15:55:27 +1000

    didn't need useeffect

--- hooks/useRecordedBoxes.js
+++ hooks/useRecordedBoxes.js
@@ -7,32 +7,29 @@ export default function useRecordedBoxes(filteredRecordings) {
     const {wakeupTime, boxSizeUnit, boxSizeNumber} = useSelector(state => state.scheduleEssentials.value);
     const overlayDimensions = useSelector(state => state.overlayDimensions.value);
     let recordingBoxes = [];
-    useEffect(() => {
-        if(filteredRecordings.length > 0) {
-            filteredRecordings.forEach(element => {
-                let pixelsToRecordingStart = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedStartTime));
-                let pixelsToRecordingEnd = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedEndTime)); 
-                let marginToRecording = pixelsToRecordingStart+overlayDimensions.headerHeight;
-                let recordingBoxHeight = pixelsToRecordingEnd-marginToRecording;
-                let availableSpace = (overlayDimensions.overlayHeight-marginToRecording);
-                let biggerThanAvailableSpace = recordingBoxHeight > availableSpace;
+    if(filteredRecordings.length > 0) {
+        filteredRecordings.forEach(element => {
+            let pixelsToRecordingStart = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedStartTime));
+            let pixelsToRecordingEnd = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedEndTime)); 
+            let marginToRecording = pixelsToRecordingStart+overlayDimensions.headerHeight;
+            let recordingBoxHeight = pixelsToRecordingEnd-marginToRecording;
+            let availableSpace = (overlayDimensions.overlayHeight-marginToRecording);
+            let biggerThanAvailableSpace = recordingBoxHeight > availableSpace;
 
-                //error handling
-                if(recordingBoxHeight < overlayDimensions.timeboxHeight) {
-                    recordingBoxHeight = overlayDimensions.timeboxHeight;
-                }else if(biggerThanAvailableSpace) { 
-                    recordingBoxHeight = availableSpace;
-                }
+            //error handling
+            if(recordingBoxHeight < overlayDimensions.timeboxHeight) {
+                recordingBoxHeight = overlayDimensions.timeboxHeight;
+            }else if(biggerThanAvailableSpace) { 
+                recordingBoxHeight = availableSpace;
+            }
 
-                let eitherIsNotZero = !(marginToRecording == 0 || recordingBoxHeight == 0); //due to overlay dimensions not being set at right time
-                let notAlreadyInCache = !recordingBoxes.some(item => item.id === element.id); //if not already in cache
+            let eitherIsNotZero = !(marginToRecording == 0 || recordingBoxHeight == 0); //due to overlay dimensions not being set at right time
+            let notAlreadyInCache = !recordingBoxes.some(item => item.id === element.id); //if not already in cache
 
-                if(eitherIsNotZero && notAlreadyInCache) { //if not already in malleableBoxes push it to it
-                    recordingBoxes.push({timeBox: element.timeBox, id: element.id, recordingBoxHeight, marginToRecording, title: element.timeBox.title});
-                }
-            });
-        }
-        
-    }, [filteredRecordings]);
+            if(eitherIsNotZero && notAlreadyInCache) { //if not already in malleableBoxes push it to it
+                recordingBoxes.push({timeBox: element.timeBox, id: element.id, recordingBoxHeight, marginToRecording, title: element.timeBox.title});
+            }
+        });
+    }  
     return recordingBoxes;
 }

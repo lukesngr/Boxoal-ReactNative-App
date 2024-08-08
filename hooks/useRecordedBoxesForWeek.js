efd95bbc7e8e740e522a05b8a89edca307516f81
Luke Sanger <lukesngr@gmail.com>
2024-08-08 15:17:14 +1000

    Moved more code into hook to simplify react components

---
+++ hooks/useRecordedBoxesForWeek.js
@@ -0,0 +1,43 @@
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculatePixelsFromTopOfGridBasedOnTime } from "../modules/coreLogic";
+import dayjs from "dayjs";
+import { filterRecordingBasedOnDay } from "../modules/coreLogic";
+
+export default function useRecordedBoxesForWeek(dayToName, recordedTimeboxes) {
+    const {wakeupTime, boxSizeUnit, boxSizeNumber} = useSelector(state => state.scheduleEssentials.value);
+    const overlayDimensions = useSelector(state => state.overlayDimensions.value);
+
+    let recordingBoxesForWeek = [];
+
+    for(let day of dayToName) {
+        let filteredRecordings = recordedTimeboxes.filter(filterRecordingBasedOnDay(day));
+        let recordingBoxesForDay = [];
+        if(filteredRecordings.length > 0) {
+            filteredRecordings.forEach(element => {
+                let pixelsToRecordingStart = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedStartTime));
+                let pixelsToRecordingEnd = calculatePixelsFromTopOfGridBasedOnTime(wakeupTime, boxSizeUnit, boxSizeNumber, overlayDimensions, dayjs(element.recordedEndTime)); 
+                let marginToRecording = pixelsToRecordingStart+overlayDimensions.headerHeight;
+                let recordingBoxHeight = pixelsToRecordingEnd-marginToRecording;
+                let availableSpace = (overlayDimensions.overlayHeight-marginToRecording);
+                let biggerThanAvailableSpace = recordingBoxHeight > availableSpace;
+
+                //error handling
+                if(recordingBoxHeight < overlayDimensions.timeboxHeight) {
+                    recordingBoxHeight = overlayDimensions.timeboxHeight;
+                }else if(biggerThanAvailableSpace) { 
+                    recordingBoxHeight = availableSpace;
+                }
+
+                let eitherIsNotZero = !(marginToRecording == 0 || recordingBoxHeight == 0); //due to overlay dimensions not being set at right time
+                let notAlreadyInCache = !recordingBoxesForDay.some(item => item.id === element.id); //if not already in cache
+
+                if(eitherIsNotZero && notAlreadyInCache) { //if not already in malleableBoxes push it to it
+                    recordingBoxesForDay.push({timeBox: element.timeBox, id: element.id, recordingBoxHeight, marginToRecording, title: element.timeBox.title});
+                }
+            });
+        }
+        recordingBoxesForWeek.push(recordingBoxesForDay); 
+    } 
+    return recordingBoxesForWeek;
+}

f5b07fd407d89e85ff01ed5a8e0aeca5f88eac80
Luke Sanger <lukesngr@gmail.com>
2024-08-08 15:35:25 +1000

    I keep on making dumb mistakes

--- components/overlay/RecordedTimeBoxOverlay.js
+++ components/overlay/RecordedTimeBoxOverlay.js
@@ -1,37 +1,32 @@
 import { View, Text } from "react-native";
 import { useSelector } from "react-redux";
-import useRecordedBoxes from "../../hooks/useRecordedBoxes";
+import useRecordedBoxes from "../../hooks/useRecordedBoxesForWeek";
 import { filterRecordingBasedOnDay } from "../../modules/coreLogic";
+import { useEffect, useMemo } from "react";
 
 export default function RecordedTimeBoxOverlay(props) {
     const onDayView = useSelector(state => state.onDayView.value);
     const daySelected = useSelector(state => state.daySelected.value);
     const {headerWidth} = useSelector(state => state.overlayDimensions.value);
     const {recordedTimeboxes} = useSelector(state => state.scheduleData.value);
-    let recordedBoxesForWeek = [];
     let displayedRecordings = [];
-    for(let day of props.dayToName) {
-        let filteredRecordings = recordedTimeboxes.filter(filterRecordingBasedOnDay(day));
-        let recordedBoxes = useRecordedBoxes(filteredRecordings);
-        recordedBoxesForWeek.push(recordedBoxes);
-    }
+    let recordedBoxesForWeek = useRecordedBoxes(props.dayToName, recordedTimeboxes);
 
     if(onDayView) {
-        displayedRecordings = recordedBoxesForWeek[daySelected];
+        displayedRecordings = [recordedBoxesForWeek[daySelected]];
+        
     }else {
         displayedRecordings = recordedBoxesForWeek;
     }
-
-    console.log(displayedRecordings);
+    
     
     return (
     <View style={{position: 'absolute', transform: [{translateX: 50}], zIndex: 999}}>
     {displayedRecordings.map((displayedRecording, index) => {
         let dayIndex = index;
-        console.log(dayIndex);
         return (
         <>
-            {displayedRecording.map((recordedBox, index) => (
+            {displayedRecording.length > 0 && displayedRecording.map((recordedBox, index) => (
                 <View key={index} style={{
                     width: headerWidth, 
                     height: recordedBox.recordingBoxHeight, 

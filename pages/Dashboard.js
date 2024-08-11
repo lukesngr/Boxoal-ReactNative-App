06bc49ee4f0103e221e8da2170d298994feae449
Luke Sanger <lukesngr@gmail.com>
2024-08-11 19:09:16 +1000

    Headline should be working now

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,7 +1,8 @@
+import { View, Text } from "react-native";
 export function Dashboard() {
   return (
     <View>
-      <Text>Dashboard</Text>
+      <Text style={{fontFamily: 'KameronRegular', fontSize: 30, color: 'black', textAlign: 'center', marginTop: 5}}>Welcome Back</Text>
     </View>
   );
 }

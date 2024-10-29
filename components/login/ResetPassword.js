cd3bf886097f3eb43f390da188a778eefc846d31
Luke Sanger <lukesngr@gmail.com>
2024-10-29 19:51:03 +1100

    everything working except final step

--- components/login/ResetPassword.js
+++ components/login/ResetPassword.js
@@ -3,8 +3,9 @@ import {styles} from '../../styles/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { confirmResetPassword, signIn } from 'aws-amplify/auth';
+import { confirmResetPassword, resetPassword } from 'aws-amplify/auth';
 import { Button, TextInput } from 'react-native-paper';
+import { set } from '../../redux/activeOverlayInterval';
 
 export function ResetPassword({navigation}) {
     const [confirmPasswordHidden, setConfirmPasswordHidden] = useState(true);
@@ -19,6 +20,7 @@ export function ResetPassword({navigation}) {
         Alert.alert("Code Sent", "Check your email for the code");
         try {
             const output = await resetPassword({ username });
+            console.log(output);
             if(output.nextStep.resetPasswordStep == 'CONFIRM_RESET_PASSWORD_WITH_CODE') {
                 setCodeSent(true);
             }
@@ -27,7 +29,8 @@ export function ResetPassword({navigation}) {
         }
     }
 
-    async function resetPassword() {
+    async function confirmAndSetPassword() {
+        setCodeSent(true);
         try {
             const output = await confirmResetPassword({ username, code, newPassword: password });
             if(output.nextStep.resetPasswordStep == 'DONE') {
@@ -37,15 +40,13 @@ export function ResetPassword({navigation}) {
         }catch(error) {
             console.log(error);
         }
+        
     }
 
     return (
         <>
             <Text style={styles.signInTitle}>Reset Password</Text>
-            {codeSent ? (<>
-                <TextInput label="Username" value={username} onChangeText={setUsername} {...styles.paperInput}></TextInput>
-                <Button mode="contained" style={{...styles.welcomeButtonOutlineStyle, marginTop: 20}} onPress={sendCode}>Send Code To SMS/Email</Button>
-            </>) : ( <>
+            {codeSent ? ( <>
                 <TextInput label="Code" value={code} onChangeText={setCode} {...styles.paperInput}></TextInput>
                 <TextInput label="Password" value={password} onChangeText={setPassword} secureTextEntry={passwordHidden} {...styles.paperInput} right={
                     <TextInput.Icon
@@ -61,8 +62,11 @@ export function ResetPassword({navigation}) {
                     forceTextInputFocus={false}
                     />}>
                 </TextInput>
-                <Button mode="contained" style={{...styles.welcomeButtonOutlineStyle, marginTop: 20}} onPress={resetPassword}>Reset Password</Button>
-            </>) 
+                <Button mode="contained" style={{...styles.welcomeButtonOutlineStyle, marginTop: 20}} onPress={confirmAndSetPassword}>Reset Password</Button>
+            </>) : (<>
+                <TextInput label="Username" value={username} onChangeText={setUsername} {...styles.paperInput}></TextInput>
+                <Button mode="contained" style={{...styles.welcomeButtonOutlineStyle, marginTop: 20}} onPress={sendCode}>Send Code To SMS/Email</Button>
+            </>)
             }
         </>
     )

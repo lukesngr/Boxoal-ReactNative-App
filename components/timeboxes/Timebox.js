2b7ffcdda31dd9034e9b26fe7eafd92462284c15
Luke Sanger <lukesngr@gmail.com>
2024-10-30 15:16:56 +1100

    Removed useless imports

--- components/timeboxes/Timebox.js
+++ components/timeboxes/Timebox.js
@@ -2,15 +2,12 @@ import { Pressable, View, Text, Modal } from "react-native"
 import NormalTimebox from "./NormalTimebox";
 import { useState } from "react";
 import { useSelector } from 'react-redux';
-import CreateTimeboxForm from "./CreateTimeboxForm";
-import TimeboxActionsForm from "./TimeboxActionsForm";
 import { useDispatch } from "react-redux";
 
 export default function Timebox(props) {
     const dispatch = useDispatch();
     const {headerWidth} = useSelector(state => state.overlayDimensions.value);
     const onDayView = useSelector(state => state.onDayView.value);
-    const [modalVisible, setModalVisible] = useState(false);
     const timeboxGrid = useSelector(state => state.timeboxGrid.value);
     const date = props.day.date+"/"+props.day.month;
     const dayName = props.day.name;

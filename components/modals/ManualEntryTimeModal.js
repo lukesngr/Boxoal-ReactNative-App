1d2119e85c3d32bbd0533d489a72fcef78c91efc
Luke Sanger <lukesngr@gmail.com>
2024-11-04 15:41:11 +1100

    Manual entry should hopefully be working

---
+++ components/modals/ManualEntryTimeModal.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { Dialog, Button } from "react-native-paper";
+import { set } from "../../redux/activeOverlayInterval";
+export default function ManualEntryTimeModal(props) {
+    const [recordedStartTime, setRecordedStartTime] = useState(props.data.startTime);
+    const [recordedEndTime, setRecordedEndTime] = useState(props.data.endTime);
+    const [startTimePickerVisible, setStartTimePickerVisible] = useState(false);
+    const [endTimePickerVisible, setEndTimePickerVisible] = useState(false);
+
+    function submitManualEntry() {
+        axios.post(serverIP+'/createRecordedTimebox', {
+            recordedStartTime, 
+            recordedEndTime,
+            timeBox: {connect: {id: props.data.id}}, 
+            schedule: {connect: {id: props.scheduleID}}
+        }).then(async () => {
+            closeModal();
+            setAlert({shown: true, title: "Timebox", message: "Added recorded timebox!"});
+            await queryClient.refetchQueries();
+        }).catch(function(error) {
+            closeModal();
+            setAlert({shown: true, title: "Error", message: "An error occurred, please try again or contact the developer"});
+            console.log(error); 
+        })  
+    }
+
+    return (<>
+    <Dialog style={{backgroundColor: '#C5C27C'}} visible={props.visible} onDismiss={props.close}>
+        <Dialog.Title style={{color: 'white'}}>Manual Entry Recorded Time For {props.datatitle}</Dialog.Title>
+        <Dialog.Content>
+            <Button textColor="black"  buttonColor="white" mode="contained" onPress={() => setStartTimePickerVisible(true)}>Pick Recorded Start Time</Button>
+            <Button textColor="black"  buttonColor="white" mode="contained" onPress={() => setEndTimePickerVisible(true)}>Pick End Start Time</Button>
+        </Dialog.Content>
+        <Dialog.Actions>
+            <Button textColor="white" onPress={closeModal}>Close</Button>
+            <Button textColor="black"  buttonColor="white" mode="contained" onPress={stopRecording}>Enter</Button>
+        </Dialog.Actions>
+    </Dialog>
+    <DatePicker modal mode="datetime" date={recordedStartTime}
+        onDateChange={(date) => setRecordedStartTime(date)}
+        open={startTimePickerVisible}
+        onConfirm={(date) => 
+            {
+                setRecordedStartTime(date)
+                setStartTimePickerVisible(false);
+            }
+        }
+        onCancel={() => setStartTimePickerVisible(false)}>
+    </DatePicker>
+    <DatePicker modal mode="datetime" date={recordedEndTime}
+        onDateChange={(date) => setRecordedEndTime(date)}
+        open={endTimePickerVisible}
+        onConfirm={(date) => 
+            {
+                setRecordedEndTime(date)
+                setEndTimePickerVisible(false);
+            }
+        }
+        onCancel={() => setEndTimePickerVisible(false)}>
+    </DatePicker>
+    </>)
+}

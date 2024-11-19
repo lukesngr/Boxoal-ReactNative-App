1be93d47616772bc649aeef24c9af4f2050d8bed
Luke Sanger <lukesngr@gmail.com>
2024-11-19 21:52:36 +1100

    removed if current day code as untestable due to cascading error from other module

--- modules/dateCode.js
+++ modules/dateCode.js
@@ -1,5 +1,4 @@
 import dayjs from "dayjs";
-import { useSelector } from "react-redux";
 
 export const dayToName = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
@@ -13,58 +12,6 @@ export function getArrayOfDayDateDayNameAndMonthForHeaders(todaysDate) {
     }
 
     return result;
-}
-
-export function getCurrentDay() {
-    const {wakeupTime} = useSelector(state => state.scheduleEssentials.value);
-    const dateObject = dayjs();
-    let currentDay = dateObject.day();
-    let cutOffDateTime = dateObject.hour(wakeupTime.split(':')[0]).minute(wakeupTime.split(':')[1]);
-
-    if(dateObject.isBefore(cutOffDateTime)) {
-        return currentDay-1;
-    }else{
-        return currentDay;
-    }
-}
-
-
-export function ifCurrentDay(number, returnIfTrue, returnIfFalse) {
-    const {wakeupTime} = useSelector(state => state.scheduleEssentials.value);
-    const dateObject = dayjs();
-    let currentDay = dateObject.day();
-    let cutOffDateTime = dateObject.hour(wakeupTime.split(':')[0]).minute(wakeupTime.split(':')[1]);
-
-    if(typeof number !== 'number') { 
-        console.log("Non-number datatype given to comparison function");
-        return returnIfFalse;
-    }
-
-    if(number == currentDay &&  cutOffDateTime.isSameOrBefore(dateObject)) {
-        return returnIfTrue;
-    }else if((number+1) == currentDay && dateObject.isBefore(cutOffDateTime)) {
-        return returnIfTrue;
-    }
-    return returnIfFalse;
-}
-
-export function ifEqualOrBeyondCurrentDay(number, returnIfTrue, returnIfFalse) {
-    const {wakeupTime} = useSelector(state => state.scheduleEssentials.value);
-    const dateObject = dayjs();
-    let currentDay = dateObject.day();
-    let cutOffDateTime = dateObject.hour(wakeupTime.split(':')[0]).minute(wakeupTime.split(':')[1]);
-
-    if(typeof number !== 'number') { 
-        console.log("Non-number datatype given to comparison function");
-        return returnIfFalse;
-    }
-    
-    if(number >= currentDay && cutOffDateTime.isSameOrBefore(dateObject)) {
-        return returnIfTrue;
-    }else if((number+1) >= currentDay && dateObject.isBefore(cutOffDateTime)) {
-        return returnIfTrue;
-    }
-    return returnIfFalse;
 }
 
 function alteredBinarySearchForTimeboxDate(timeboxes, selectedDate) {

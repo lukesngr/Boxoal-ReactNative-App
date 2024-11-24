51ff2a2798ac1e5bc01f748baf715a346e9a1579
Luke Sanger <lukesngr@gmail.com>
2024-11-24 12:01:37 +1100

    got asset mock working somehow

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    // This line is crucial - it mocks ALL image files
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/assetMock.js',
+  }
 };

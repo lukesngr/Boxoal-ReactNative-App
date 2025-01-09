198e9d5cf2ae5ed7c462ddcd871a687abd7cab07
Luke Sanger <lukesngr@gmail.com>
2025-01-09 20:41:20 +1100

    removed anything associated with old state of schedule

--- components/schedules/EditScheduleForm.js
+++ components/schedules/EditScheduleForm.js
@@ -14,23 +14,12 @@ import { useDispatch, useSelector } from "react-redux";
 export default function EditScheduleForm(props) {
     const dispatch = useDispatch();
     const [title, setTitle] = useState(props.data.title);
-    const [boxSizeNumber, setBoxSizeNumber] = useState(props.data.boxSizeNumber.toString());
-    const [boxSizeUnit, setBoxSizeUnit] = useState(props.data.boxSizeUnit);
-    let wakeupDateTime = new Date();
-    wakeupDateTime.setHours(props.data.wakeupTime.split(':')[0]);
-    wakeupDateTime.setMinutes(props.data.wakeupTime.split(':')[1]);
-    const [wakeupTime, setWakeupTime] = useState(wakeupDateTime);
-    const [wakeupTimeText, setWakeupTimeText] = useState(props.data.wakeupTime);
-    const [wakeupTimeModalVisible, setWakeupTimeModalVisible] = useState(false);
     const [alert, setAlert] = useState({shown: false, title: "", message: ""});
     const selectedSchedule = useSelector(state => state.selectedSchedule.value);
     
     async function updateSchedule() {
         axios.put(serverIP+'/updateSchedule', {
             title,
-            boxSizeNumber: parseInt(boxSizeNumber),
-            boxSizeUnit,
-            wakeupTime: wakeupTimeText,
             id: props.data.id, 
         }).then(async () => {
             props.close();
@@ -68,23 +57,7 @@ export default function EditScheduleForm(props) {
             <Dialog.Title style={{color: 'white'}}>Edit Schedule</Dialog.Title>
             <Dialog.Content>
                 <TextInput label="Title" value={title} onChangeText={setTitle} {...styles.paperInput}/>
-                <TextInput label="Timebox Duration" value={boxSizeNumber} onChangeText={setBoxSizeNumber} {...styles.paperInput}/>
-                <TextInput label="Timebox Unit"  value={boxSizeUnit} {...styles.paperInput}
-	                render={(props) => (
-                        <Picker style={{color: 'black', marginTop: 5}} dropdownIconColor='black' selectedValue={boxSizeUnit} onValueChange={setBoxSizeUnit}>
-                            <Picker.Item label="Min" value="min" />
-                            <Picker.Item label="Hour" value="hr" />
-                        </Picker>
-	                )}
-                />
-                <Pressable onPress={() => setWakeupTimeModalVisible(true)}>
-                    <TextInput 
-                    label="Wakeup time" 
-                    value={wakeupTimeText}
-                    right={<TextInput.Icon onPress={() => setWakeupTimeModalVisible(true)} icon="clock-edit" />} 
-                    editable={false} 
-                    {...styles.paperInput}/>
-                </Pressable>
+                
             </Dialog.Content>
             <Dialog.Actions>
                 <Button textColor="white" onPress={props.close}>Close</Button>
@@ -94,23 +67,5 @@ export default function EditScheduleForm(props) {
           </Dialog>
             {alert.shown && <Alert visible={alert.shown} close={() => setAlert({...alert, shown: false})} title={alert.title} message={alert.message}/> }
         </Portal>
-        <DatePicker 
-            modal 
-            mode="time" 
-            date={wakeupTime} 
-            onDateChange={
-                (date) => {
-                    setWakeupTime(date);
-                    setWakeupTimeText(convertToTimeAndDate(date)[0]);
-                }
-            } 
-            open={wakeupTimeModalVisible} 
-            onConfirm={(date) => { 
-                setWakeupTime(date); 
-                setWakeupTimeModalVisible(false);
-                setWakeupTimeText(convertToTimeAndDate(date)[0]);
-            }} 
-            onCancel={() => setWakeupTimeModalVisible(false)}>
-        </DatePicker>
     </>)
 }

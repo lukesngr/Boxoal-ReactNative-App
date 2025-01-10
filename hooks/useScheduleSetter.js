127906ae62f775fcf39806567179c67cc9a2601c
Luke Sanger <lukesngr@gmail.com>
2025-01-10 18:17:32 +1100

    forgot to set schedule id in profile

--- hooks/useScheduleSetter.js
+++ hooks/useScheduleSetter.js
@@ -1,13 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 export function useScheduleSetter(schedule) {
     const dispatch = useDispatch();
+    const profile = useSelector(state => state.profile.value);
     useEffect(() => {
+        dispatch({type: 'profile/set', payload: {id: schedule.id, ...profile}});
         dispatch({type: 'scheduleData/set', payload: {timeboxes: schedule.timeboxes, recordedTimeboxes: schedule.recordedTimeboxes, goals: schedule.goals}});
     }, []);
 
     useEffect(() => {
+        dispatch({type: 'profile/set', payload: {id: schedule.id, ...profile}});
         dispatch({type: 'scheduleData/set', payload: {timeboxes: schedule.timeboxes, recordedTimeboxes: schedule.recordedTimeboxes, goals: schedule.goals}});
     }, [schedule]);
 }

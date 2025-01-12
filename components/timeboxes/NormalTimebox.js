5455c344f5ecc7afaf4b563c9bb1f9cfde03e660
Luke Sanger <lukesngr@gmail.com>
2025-01-12 19:06:05 +1100

    this should make timebox expand if schema set to something smaller than size of timebox

--- components/timeboxes/NormalTimebox.js
+++ components/timeboxes/NormalTimebox.js
@@ -1,12 +1,15 @@
 import { Text, View } from "react-native";
 import { styles } from "../../styles/styles";
 import { useSelector } from "react-redux";
+import { getPercentageOfBoxSizeFilled } from "../../modules/boxCalculations";
 
 export default function NormalTimebox(props) {
     const onDayView = useSelector(state => state.onDayView.value);
+    const {boxSizeNumber, boxSizeUnit} = useSelector(state => state.profile.value);
     let fontSize = onDayView ? 20 : 12;
     let timeboxHeight = onDayView ? styles.enlargedTimeboxHeight : styles.normalTimeboxHeight;
-    let height = timeboxHeight*props.data.numberOfBoxes;
+    let percentageOfBoxSizeFilled = getPercentageOfBoxSizeFilled(boxSizeUnit, boxSizeNumber, props.data.startTime, props.data.endTime);
+    let height = timeboxHeight*props.data.numberOfBoxes*percentageOfBoxSizeFilled;
 
     return (
     <View style={{position: 'relative', height: height, backgroundColor: props.data.color, width: '100%'}}>

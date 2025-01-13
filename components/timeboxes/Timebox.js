1028b9bda26bb1c5b2b988e1b9c88191fd6abcea
Luke Sanger <lukesngr@gmail.com>
2025-01-13 18:38:41 +1100

    Set foundations that if box has two boxes that it displays confused sybol and if one timebox but in middle of range it has a margin

--- components/timeboxes/Timebox.js
+++ components/timeboxes/Timebox.js
@@ -3,19 +3,32 @@ import NormalTimebox from "./NormalTimebox";
 import { useState } from "react";
 import { useSelector } from 'react-redux';
 import { useDispatch } from "react-redux";
+import { filterTimeGridBasedOnSpace, getMarginFromTopOfTimebox } from "../../modules/boxCalculations";
 
 export default function Timebox(props) {
     const dispatch = useDispatch();
     const {headerWidth} = useSelector(state => state.overlayDimensions.value);
     const onDayView = useSelector(state => state.onDayView.value);
     const timeboxGrid = useSelector(state => state.timeboxGrid.value);
+    const {boxSizeNumber, boxSizeUnit} = useSelector(state => state.profile.value);
     const date = props.day.date+"/"+props.day.month;
     const dayName = props.day.name;
     let data;
+    let marginFromTop = 0;
+    let moreThanOneBoxesInSpace = false;
 
     if(timeboxGrid) { 
         if(timeboxGrid[date]) {
-            data = timeboxGrid[date][props.time];
+            let boxesInsideSpace = filterTimeGridBasedOnSpace(timeboxGrid[date], boxSizeUnit, boxSizeNumber, props.time);
+            if(boxesInsideSpace.length > 1) {
+                moreThanOneBoxesInSpace = true;
+            }
+
+            if(timeboxGrid[date][props.time]) {
+                data = timeboxGrid[date][props.time];
+            }else{
+                marginFromTop = getMarginFromTopOfTimebox(boxSizeUnit, boxSizeNumber, props.time, boxesInsideSpace[0]);
+            }
         }
     }
 
@@ -30,7 +43,7 @@ export default function Timebox(props) {
     return (
     <View style={{borderWidth: 1, borderColor: 'black', width: onDayView ? headerWidth : 50.5, height: onDayView ? 60 : 30, zIndex: 998}}>
         <Pressable onPress={onPress}>
-            {data ? (<NormalTimebox data={data}></NormalTimebox>) : (<Text style={{width: '100%', height: '100%'}}></Text>)}
+            {data ? (<NormalTimebox marginFromTop={marginFromTop} data={data}></NormalTimebox>) : (<Text style={{width: '100%', height: '100%'}}></Text>)}
         </Pressable>
     </View>
     )

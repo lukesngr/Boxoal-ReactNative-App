03009d6d6d700a73a22dcd8e77cd6ffb63d5a835
Luke Sanger <lukesngr@gmail.com>
2025-01-14 19:52:27 +1100

    MAde conditional hook, dumb idea

--- hooks/useProfile.js
+++ hooks/useProfile.js
@@ -1,20 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { getProgressAndLevel } from "../modules/coreLogic";
-export function useProfile(userID, dispatch) {
-    const {status, data, error, refetch} = useQuery({
-        queryKey: ["XP"], 
-        queryFn: async () => {
-            const response = await axios.get(serverIP+"/getProfile", { params: {userUUID: userID}});
-            return response.data;
-        },
-        enabled: true
-    })
+export function useProfile(user, dispatch) {
+    if(user !== undefined && user.userId !== undefined) {
+        const {status, data, error, refetch} = useQuery({
+            queryKey: ["XP"], 
+            queryFn: async () => {
+                const response = await axios.get(serverIP+"/getProfile", { params: {userUUID: userID}});
+                return response.data;
+            },
+            enabled: true
+        })
 
-    if(data !== undefined) {
-        let {points, boxSizeUnit, boxSizeNumber, wakeupTime, scheduleID} = data;
-        let {progress, level} = getProgressAndLevel(points);
-        dispatch({type: 'profile/set', payload: {scheduleID, boxSizeUnit, boxSizeNumber, wakeupTime, progress, level}});
+        if(data !== undefined) {
+            let {points, boxSizeUnit, boxSizeNumber, wakeupTime, scheduleID} = data;
+            let {progress, level} = getProgressAndLevel(points);
+            dispatch({type: 'profile/set', payload: {scheduleID, boxSizeUnit, boxSizeNumber, wakeupTime, progress, level}});
+        }
     }
     return;
 }

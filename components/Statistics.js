217e1d41f430b664ca5e4bc9fa1a1b6a5802b9af
Luke Sanger <lukesngr@gmail.com>
2025-01-18 22:06:31 +1100

    Got layout right, now time to rearrange progress indicator

--- components/Statistics.js
+++ components/Statistics.js
@@ -7,13 +7,15 @@ export function Statistics(props) {
     let {averageTimeOverBy, averageTimeStartedOffBy, percentagePredictedStart, percentageCorrectTime, percentageRescheduled} = getStatistics(props.recordedTimeboxes);
 
     return (
-        <Surface style={{backgroundColor: 'white', height: 'fit-content', width: '80%', marginTop: 4, marginHorizontal: 30}} elevation={4}>
+        <Surface style={{backgroundColor: '#C5C27C', height: 'fit-content', width: '80%', marginTop: 30, marginHorizontal: 30}} elevation={4}>
             <Text style={{fontFamily: 'KameronRegular', fontSize: 25, color: 'black', marginTop: 20, marginHorizontal: 30}}>Statistics</Text>
             <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Over By: {averageTimeOverBy}min</Text>
             <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Started Off By: {averageTimeStartedOffBy}min</Text>
-            <StatisticsProgressIndicator percentage={percentagePredictedStart} text="Predicted Start"/>
-            <StatisticsProgressIndicator percentage={percentageCorrectTime} text="Correct Time"/>
-            <StatisticsProgressIndicator percentage={percentageRescheduled} text="Rescheduled"/>
+            <View style={{flexDirection: 'row', justifyContent: 'space-around', marginTop: 20, marginBottom: 20}}>
+                <StatisticsProgressIndicator percentage={percentagePredictedStart} text="Predicted Start"/>
+                <StatisticsProgressIndicator percentage={percentageCorrectTime} text="Correct Time"/>
+                <StatisticsProgressIndicator percentage={percentageRescheduled} text="Rescheduled"/>
+            </View>
         </Surface>
     )
 }

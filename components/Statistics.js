812d46448ff663105a4eb000294be75884a271fd
Luke Sanger <lukesngr@gmail.com>
2025-01-18 21:51:32 +1100

    TRying different style of displaying graphs

--- components/Statistics.js
+++ components/Statistics.js
@@ -11,9 +11,9 @@ export function Statistics(props) {
             <Text style={{fontFamily: 'KameronRegular', fontSize: 25, color: 'black', marginTop: 20, marginHorizontal: 30}}>Statistics</Text>
             <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Over By: {averageTimeOverBy}min</Text>
             <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Started Off By: {averageTimeStartedOffBy}min</Text>
-            <StatisticsProgressIndicator percentage={percentagePredictedStart} text="Percentage That Is Correct Start"/>
-            <StatisticsProgressIndicator percentage={percentageCorrectTime} text="Percentage That Is Correct Length"/>
-            <StatisticsProgressIndicator percentage={percentageRescheduled} text="Percentage That is Rescheduled"/>
+            <StatisticsProgressIndicator percentage={percentagePredictedStart} text="Predicted Start"/>
+            <StatisticsProgressIndicator percentage={percentageCorrectTime} text="Correct Time"/>
+            <StatisticsProgressIndicator percentage={percentageRescheduled} text="Rescheduled"/>
         </Surface>
     )
 }

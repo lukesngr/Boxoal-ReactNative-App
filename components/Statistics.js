f894889e30cc099235c2e8e9bc7cee0c09328425
Luke Sanger <lukesngr@gmail.com>
2025-01-18 20:13:20 +1100

    MAde font a little bit more smaller and readable

--- components/Statistics.js
+++ components/Statistics.js
@@ -1,4 +1,4 @@
-import { Text } from "react-native-paper";
+import { Text, Surface } from "react-native-paper";
 import { getStatistics } from "../modules/boxCalculations";
 import { View } from "react-native";
 import { StatisticsProgressIndicator } from "./StatisticsProgressIndicator";
@@ -7,12 +7,13 @@ export function Statistics(props) {
     let {averageTimeOverBy, averageTimeStartedOffBy, percentagePredictedStart, percentageCorrectTime, percentageRescheduled} = getStatistics(props.recordedTimeboxes);
 
     return (
-        <View>
-            <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Over By: {averageTimeOverBy}min</Text>
-            <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Started Off By: {averageTimeStartedOffBy}min</Text>
+        <Surface style={{backgroundColor: 'white', height: 'fit-content', width: '80%', marginTop: 4, marginHorizontal: 30}} elevation={4}>
+            <Text style={{fontFamily: 'KameronRegular', fontSize: 25, color: 'black', marginTop: 20, marginHorizontal: 30}}>Statistics</Text>
+            <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Over By: {averageTimeOverBy}min</Text>
+            <Text style={{fontFamily: 'Roboto', fontSize: 15, color: 'black', marginTop: 20, marginHorizontal: 30}}>Average Time Started Off By: {averageTimeStartedOffBy}min</Text>
             <StatisticsProgressIndicator percentage={percentagePredictedStart} text="Percentage That Is Correct Start"/>
             <StatisticsProgressIndicator percentage={percentageCorrectTime} text="Percentage That Is Correct Length"/>
             <StatisticsProgressIndicator percentage={percentageRescheduled} text="Percentage That is Rescheduled"/>
-        </View>
+        </Surface>
     )
 }

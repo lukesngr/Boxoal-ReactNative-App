cae2bddad150568144d084c0d5adf8ca0e42c2ff
Luke Sanger <lukesngr@gmail.com>
2025-01-21 16:41:35 +1100

    Added video and fixed some dependencies, looking good so far

--- components/Welcome.js
+++ components/Welcome.js
@@ -5,21 +5,18 @@ import { Modal, View} from "react-native";
 import CreateScheduleForm from "./schedules/CreateScheduleForm";
 import { styles } from "../styles/styles";
 import { Avatar, Card, Paragraph } from "react-native-paper";
+import Video from 'react-native-video';
+import intro from '../assets/intro.mp4';
 
 export default function Welcome() {
     
     const [modalVisible, setModalVisible] = useState(false);
     return (<>
         <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%'}}>
-            <Card style={{backgroundColor: 'white', margin: 10, marginTop: 100}} mode={'elevated'}>
+            <Card style={{backgroundColor: 'white', margin: 10, marginTop: 50}} mode={'elevated'}>
                 <Card.Title title="Welcome to Boxoal" subtitle="Tutorial video" left={(props) => <Avatar.Icon {...props} icon="information" />}/>
-                <Card.Cover style={{borderRadius: 0}}/>
-                <Card.Title title="Instructions" subtitle="How to get started"/>
-                <Card.Content style={{marginTop: 30}}>
-                    <Paragraph variant="bodyMedium">
-                        First start by creating a schedule with the button below, then create a goal, then add timeboxes to said goal
-                    </Paragraph>
-                </Card.Content>
+                <Video source={intro} style={{width: '100%', height: 500}} paused={false} repeat={true} controls={true}></Video>
+                <Card.Title title="Instructions" subtitle="Create a schedule to get started"/>
                 <Card.Actions>
                     <Button mode='contained' onPress={() => setModalVisible(true)}>Create Schedule</Button>
                 </Card.Actions>

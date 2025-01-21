611ef07eb93dc950cd0747d2194bfa1e6b6966d9
Luke Sanger <lukesngr@gmail.com>
2025-01-21 16:43:00 +1100

    Looks perfect moving onto to new features, just need to design first

--- components/Welcome.js
+++ components/Welcome.js
@@ -15,7 +15,7 @@ export default function Welcome() {
         <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%'}}>
             <Card style={{backgroundColor: 'white', margin: 10, marginTop: 50}} mode={'elevated'}>
                 <Card.Title title="Welcome to Boxoal" subtitle="Tutorial video" left={(props) => <Avatar.Icon {...props} icon="information" />}/>
-                <Video source={intro} style={{width: '100%', height: 500}} paused={false} repeat={true} controls={true}></Video>
+                <Video source={intro} style={{width: '100%', height: 500, backgroundColor: 'black'}} paused={false} repeat={true} controls={true}></Video>
                 <Card.Title title="Instructions" subtitle="Create a schedule to get started"/>
                 <Card.Actions>
                     <Button mode='contained' onPress={() => setModalVisible(true)}>Create Schedule</Button>

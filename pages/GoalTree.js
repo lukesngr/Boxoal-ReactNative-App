2ebe6ed098ab0ea1f4bf64c0fa88fabc7b410dc5
Luke Sanger <lukesngr@gmail.com>
2025-01-24 09:28:27 +1100

    set base only thing left is the graph in question

--- pages/GoalTree.js
+++ pages/GoalTree.js
@@ -1,8 +1,33 @@
-export function GoalTree() {
+import { IconButton } from "react-native-paper";
+import { getMaxNumberOfGoals } from "../modules/coreLogic";
+
+export function GoalTree(props) {
+    const [currentLine, setCurrentLine] = useState(1);
+    let goalsCompleted = props.data.goals.reduce((count, item) => item.completed ? count + 1 : count, 0);
+    let maxNumberOfGoals = getMaxNumberOfGoals(goalsCompleted);
+
+    function moveLeft() {
+        if(currentLine > 1) {
+            setCurrentLine(currentLine - 1);
+        }else if(currentLine == 1) {
+            setCurrentLine(maxNumberOfGoals);
+        }
+    }
+
+    function moveRight() {
+        if(currentLine < maxNumberOfGoals) {
+            setCurrentLine(currentLine + 1);
+        }else if(currentLine == maxNumberOfGoals) {
+            setCurrentLine(1);
+        }
+    }
     return (
     <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%', padding: 20, paddingLeft: 15, paddingRight: 15}}>
         <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
             <Text style={{fontFamily: 'KameronRegular', fontSize: 30, color: 'black', textAlign: 'center', marginTop: 30}}>Goal Tree</Text>
+            {maxNumberOfGoals > 1 && <IconButton icon="arrow-left" size={36} onPress={() => moveLeft()}></IconButton> }
+            <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black', marginTop: 20, marginHorizontal: 30}}>Goal {currentLine}</Text>
+            {maxNumberOfGoals > 1 && <IconButton icon="arrow-right" size={36} onPress={() => moveRight()}></IconButton> }
         </View>
     </View>)
 }

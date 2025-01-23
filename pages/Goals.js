75ce0ea32d457432f7ec8a3736f5e2e9121bca24
Luke Sanger <lukesngr@gmail.com>
2025-01-24 09:30:36 +1100

    Set tree button to open goaltree

--- pages/Goals.js
+++ pages/Goals.js
@@ -9,21 +9,23 @@ import GoalAccordion from "../components/goals/GoalAccordion";
 import CreateGoalForm from "../components/goals/CreateGoalForm";
 import { styles } from "../styles/styles";
 import CorrectModalDisplayer from "../components/modals/CorrectModalDisplayer";
+import { GoalTree } from "./GoalTree";
 
 export default function Goals(props) {
     const profile = useSelector(state => state.profile.value);
     const [createScheduleVisible, setCreateScheduleVisible] = useState(false);
     const [editScheduleVisible, setEditScheduleVisible] = useState(false);
     const [createGoalVisible, setCreateGoalVisible] = useState(false);
+    const [showSkillTree, setShowSkillTree] = useState(false);
     let schedule = props.data[profile.scheduleIndex];
 
-    return (<>
+    return (showSkillTree ? (<GoalTree close={setShowSkillTree}></GoalTree>) : (<>
         <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%', padding: 20, paddingLeft: 15, paddingRight: 15}}>
             <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
                 <Surface style={{flexDirection: 'row', alignItems: 'center', paddingLeft: 20, backgroundColor: 'white'}}> 
                     <Text style={{fontSize: 25, color: 'black', textAlign: 'left', marginTop: 5, width: 220}}>{schedule.title}</Text>
                     <View style={{flexDirection: 'row', justifyContent: 'space-between', gap: -25}}>
-                        <IconButton icon="tree" size={36} onPress={() => props.close()}></IconButton>
+                        <IconButton icon="tree" size={36} onPress={setShowSkillTree(true)}></IconButton>
                         <IconButton icon="cog" size={36} onPress={() => setEditScheduleVisible(true)}></IconButton>
                         <IconButton icon="plus" size={36} onPress={() => setCreateScheduleVisible(true)}></IconButton>
                     </View>
@@ -40,6 +42,6 @@ export default function Goals(props) {
         <CreateGoalForm visible={createGoalVisible} close={() => setCreateGoalVisible(false)} id={schedule.id}  goals={schedule.goals}></CreateGoalForm>
         <EditScheduleForm data={schedule} visible={editScheduleVisible} close={() => setEditScheduleVisible(false)}></EditScheduleForm>
         <CreateScheduleForm visible={createScheduleVisible} close={() => setCreateScheduleVisible(false)}></CreateScheduleForm>
-        </>
+        </>)
     )
 }

aa3ee4c84fedb6586e55571bcbbc959c94bfec0b
Luke Sanger <lukesngr@gmail.com>
2025-01-23 12:39:21 +1100

    Code completed, might need to optimize but so far so good

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -11,29 +11,20 @@ export function Dashboard(props) {
 
   const {scheduleIndex} = useSelector(state => state.profile.value);
   let userID = props.userID;
-  let recordedTimeboxes = props.data[scheduleIndex].recordedTimeboxes;
-  const {status, data, error, refetch} = useQuery({
-    queryKey: ["XP"], 
-    queryFn: async () => {
-        const response = await axios.get(serverIP+"/getExperience", { params: {userUUID: userID}});
-        return response.data;
-    },
-    enabled: true
-  })
-  let currentLevel = 1;
-  let currentProgress = 0;
-
-  if(data !== undefined) {
-    let {progress, level} = getProgressAndLevel(data.points);
-    currentLevel = level;
-    currentProgress = progress;
+  let data = props.data[scheduleIndex]
+  let goalsCompleted = data.goals.reduce((count, item) => item.completed ? count + 1 : count, 0);
+  let averageProgress = 0;
+  for(let goal of data.goals) {
+    averageProgress += getProgressWithGoal(goal.timeboxes);
   }
+  averageProgress = averageProgress / data.goals.length;
+  let recordedTimeboxes = props.data[scheduleIndex].recordedTimeboxes;
 
   return (
     <View style={{backgroundColor: '#D9D9D9', height: '100%'}}>
       <Text style={{fontFamily: 'KameronRegular', fontSize: 30, color: 'black', textAlign: 'center', marginTop: 30}}>Welcome Back</Text>
-      <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black', marginTop: 20, marginHorizontal: 30}}>Lvl {currentLevel}</Text>
-      <ProgressBar progress={currentProgress} color={'#C5C27C'} style={{marginTop: 10, marginHorizontal: 30, width: '80%'}} />
+      <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black', marginTop: 20, marginHorizontal: 30}}>Lvl {goalsCompleted}</Text>
+      <ProgressBar progress={averageProgress} color={'#C5C27C'} style={{marginTop: 10, marginHorizontal: 30, width: '80%'}} />
       <Statistics recordedTimeboxes={recordedTimeboxes} />
     </View>
   );

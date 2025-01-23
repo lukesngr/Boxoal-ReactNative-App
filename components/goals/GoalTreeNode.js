995c9ac0a013ae4a2c33ad4706785ef26416fee0
Luke Sanger <lukesngr@gmail.com>
2025-01-24 10:32:55 +1100

    just needed to add align self

--- components/goals/GoalTreeNode.js
+++ components/goals/GoalTreeNode.js
@@ -1,7 +1,7 @@
 import { Surface, Text } from "react-native-paper"
 export function GoalTreeNode(props) {
     return (
-        <Surface style={{padding: 10, margin: 10, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Surface style={{padding: 10, margin: 10, backgroundColor: '#D9D9D9', borderRadius: 25, alignSelf: 'center'}}>
             <Text style={{fontFamily: 'KameronRegular', fontSize: 20, color: 'black'}}>{props.goal.title}</Text>
         </Surface>
     )

e4e03f7cea02f1270432194ca8bdf4f40e00d74b
Luke Sanger <lukesngr@gmail.com>
2025-01-24 09:18:59 +1100

    Building base of goal tree page

---
+++ pages/GoalTree.js
@@ -0,0 +1,8 @@
+export function GoalTree() {
+    return (
+    <View style={{backgroundColor: '#D9D9D9', width: '100%', height: '100%', padding: 20, paddingLeft: 15, paddingRight: 15}}>
+        <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
+            <Text style={{fontFamily: 'KameronRegular', fontSize: 30, color: 'black', textAlign: 'center', marginTop: 30}}>Goal Tree</Text>
+        </View>
+    </View>)
+}

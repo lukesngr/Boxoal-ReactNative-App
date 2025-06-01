a6a18d9a21254cb50fae747b59d3b9ae37ba9b4f
Luke Sanger <lukesngr@gmail.com>
2025-06-01 15:59:01 +1000

    previous commit

--- components/StatisticsGraph.js
+++ components/StatisticsGraph.js
@@ -1,6 +1,8 @@
 import Svg, { Circle, Text } from "react-native-svg";
 import { View } from "react-native";
 import PieChart from 'react-native-pie-chart'
+import { Surface } from "react-native-paper";
+import { styles } from "../styles/styles";
 
 export function StatisticsGraph(props) {
     let {percentage, text} = props;
@@ -11,8 +13,10 @@ export function StatisticsGraph(props) {
       { value: 1-percentage, color: '#495057' },
     ]
     return (
-        <View style={{marginLeft: 10, marginRight: 10}}>             
-            <PieChart widthAndHeight={widthAndHeight} series={series} />
-        </View>
+        <Surface style={{backgroundColor: styles.primaryColor, height: 'fit-content', width: '80%', marginTop: 30, marginHorizontal: 30}}  elevation={4}>
+            <View style={{marginLeft: 10, marginRight: 10}}>             
+                <PieChart widthAndHeight={widthAndHeight} series={series} />
+            </View>
+        </Surface>
         )
 }

5d30592e830c3adb26e948dfd55759a454e2d388
Luke Sanger <lukesngr@gmail.com>
2025-06-01 15:55:03 +1000

    Added pie chart now time to make nice

--- components/StatisticsGraph.js
+++ components/StatisticsGraph.js
@@ -1,39 +1,18 @@
 import Svg, { Circle, Text } from "react-native-svg";
 import { View } from "react-native";
+import PieChart from 'react-native-pie-chart'
 
 export function StatisticsGraph(props) {
     let {percentage, text} = props;
-    const size = 200;
-    let strokeWidth = 4;
-    const radius = (size - strokeWidth) / 2;
-    const circumference = 2 * Math.PI * radius;
-    const strokeDashoffset = circumference - (circumference * percentage);
+    const widthAndHeight = 100
+
+    const series = [
+      { value: percentage, color: '#000' },
+      { value: 1-percentage, color: '#495057' },
+    ]
     return (
         <View style={{marginLeft: 10, marginRight: 10}}>             
-    <Svg width={size} height={size}>                 
-        <Circle                     
-            stroke="#D9D9D9"                     
-            fill="none"                     
-            cx={size / 2}                     
-            cy={size / 2}                     
-            r={radius}                     
-            strokeWidth={strokeWidth}                 
-        />                  
-        <Circle                     
-            stroke="#2f4858"                     
-            fill="#2f4858"                     
-            cx={size / 2}                     
-            cy={size / 2}                     
-            r={radius - strokeWidth/2}                     
-            strokeWidth={0}                     
-            strokeDasharray={circumference}                     
-            strokeDashoffset={strokeDashoffset}                     
-            strokeLinecap="round"                     
-            rotation="-90"                     
-            originX={size / 2}                     
-            originY={size / 2}                 
-        />             
-    </Svg>         
-</View>
+            <PieChart widthAndHeight={widthAndHeight} series={series} />
+        </View>
         )
 }

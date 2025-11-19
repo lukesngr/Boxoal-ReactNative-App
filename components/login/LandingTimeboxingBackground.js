8d2a7140f92c558b3c870aee11b65b530d9a3e89
Luke Sanger <lukesngr@gmail.com>
2025-11-19 14:29:47 +1100

    got landing background now time to add to add forms and finally complete this bitch

--- components/login/LandingTimeboxingBackground.js
+++ components/login/LandingTimeboxingBackground.js
@@ -173,8 +173,9 @@ export function LandingTimeboxingBackground(props) {
     </View>
     <BlurView blurType="light" style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, height: '100%', width: '100%'}}
         blurAmount={1000}>
+        {props.children}
     </BlurView>
-    {props.children}
+    
     </>
     )
 }

f967431d3484776bdcd471cbb7288f693642a875
Luke Sanger <lukesngr@gmail.com>
2025-11-19 13:47:14 +1100

    getting this shit to compile was a major pain in my ass

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-const {
- withSentryConfig
-} = require("@sentry/react-native/metro");
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
-module.exports = withSentryConfig(mergeConfig(getDefaultConfig(__dirname), config));
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);

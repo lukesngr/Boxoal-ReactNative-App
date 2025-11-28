2d2b7ae7352bd0b91c086aee541359d814785596
Luke Sanger <lukesngr@gmail.com>
2025-11-28 21:26:03 +1100

    removed console log

--- hooks/useProfile.js
+++ hooks/useProfile.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import { getProgressAndLevel } from "../modules/coreLogic";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import serverIP from "../modules/serverIP";
 export function useProfile(userId, dispatch) {
-
+    
     const {status, data, error, refetch} = useQuery({
         queryKey: ["XP"], 
         queryFn: async () => {
@@ -16,10 +17,11 @@ export function useProfile(userId, dispatch) {
 
     useEffect(() => {
         if(data !== undefined) {
-            let {boxSizeUnit, boxSizeNumber, wakeupTime, scheduleID} = data;
-            dispatch({type: 'profile/set', payload: {scheduleID, boxSizeUnit, boxSizeNumber, wakeupTime}});
+            let {boxSizeUnit, boxSizeNumber, wakeupTime, scheduleID, goalLimit, scheduleIndex} = data;
+            dispatch({type: 'profile/set', payload: {scheduleID, scheduleIndex, boxSizeUnit, boxSizeNumber, wakeupTime, goalLimit}});
         }else{
-            dispatch({type: 'profile/set', payload: {scheduleID: 0, scheduleIndex: 0, boxSizeUnit: 'min', boxSizeNumber: 30, wakeupTime: '07:00'}});
+            console.log(data, userId)
+            dispatch({type: 'profile/set', payload: {scheduleID: 0, scheduleIndex: 0, boxSizeUnit: 'min', boxSizeNumber: 30, wakeupTime: '07:00', goalLimit: -1}});
         }
     }, [data]);
     

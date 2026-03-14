e2aeb19d3068add2914acc279cbe69ce407e4b9e
Luke Sanger <lukesngr@gmail.com>
2026-03-14 15:42:36 +1100

    mirrored same changes to auth

--- pages/Auth.js
+++ pages/Auth.js
@@ -4,21 +4,16 @@ import { LandingTimeboxingBackground } from '../components/login/LandingTimeboxi
 import LandingPage from '../components/login/LandingPage';
 import { Login } from '../components/login/Login';
 import { ResetPassword } from '../components/login/ResetPassword';
-import Alert from "../components/Alert";
-import { useSelector } from 'react-redux';
 
 export default function Auth({navigation}) {
     const { authStatus } = useAuthenticator();
     const [componentDisplayed, setComponentDisplayed] = useState("landing");
-    const {open, title, message} = useSelector(state => state.alert.value);
-
     useEffect(() => {
         if(authStatus == 'authenticated') { navigation.navigate('FinalView'); }
     }, [authStatus]);
 
     return (
     <>
-        <Alert open={open} title={title} message={message} close={() => dispatch({type: 'alert/set', payload: { open: false, title: "", message: "" }})}></Alert>
         <LandingTimeboxingBackground>
         {componentDisplayed == "landing" && <LandingPage setComponentDisplayed={setComponentDisplayed} />}
         {componentDisplayed == "signIn" && <Login setComponentDisplayed={setComponentDisplayed} />}
